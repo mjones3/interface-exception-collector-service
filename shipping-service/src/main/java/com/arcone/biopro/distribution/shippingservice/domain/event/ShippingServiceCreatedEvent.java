67131b189c9c9ae2cda3e789c1582c30f99ed0e1
Allan Morelli Braga <allan.morellibraga@arc-one.com>
2024-05-16 14:26:55 +0000

    Revert "Merge branch 'enabler/shipping-service-setup' into 'main'"
    This reverts merge request !1

--- shipping-service/src/main/java/com/arcone/biopro/distribution/shippingservice/domain/event/ShippingServiceCreatedEvent.java
+++
@@ -1,6 +0,0 @@
-package com.arcone.biopro.distribution.shippingservice.domain.event;
-
-import java.io.Serializable;
-
-public record ShippingServiceCreatedEvent(Long id) implements Serializable {
-}
