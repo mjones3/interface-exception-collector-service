efd23e631e0fecfde20167207b4dd89a103215cc
Allan Morelli Braga <allan.morellibraga@arc-one.com>
2024-06-13 13:11:34 +0000

    DIS-51 - Print Shipping Label - Back-end

--- shipping-service/src/main/java/com/arcone/biopro/distribution/shippingservice/domain/service/ShipmentLabelService.java
+++ shipping-service/src/main/java/com/arcone/biopro/distribution/shippingservice/domain/service/ShipmentLabelService.java
@@ -1,9 +1,11 @@
 package com.arcone.biopro.distribution.shippingservice.domain.service;
 
 import com.arcone.biopro.distribution.shippingservice.application.dto.PackingListLabelDTO;
+import com.arcone.biopro.distribution.shippingservice.application.dto.ShippingLabelDTO;
 import reactor.core.publisher.Mono;
 
 public interface ShipmentLabelService {
     Mono<PackingListLabelDTO> generatePackingListLabel(Long shipmentId);
+    Mono<ShippingLabelDTO> generateShippingLabel(Long shipmentId);
 
 }
