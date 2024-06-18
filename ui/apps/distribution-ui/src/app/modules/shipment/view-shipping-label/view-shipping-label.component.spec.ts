f5613d49cd074d85d33c597f832f776f2e5bad7e
Ram <ram.bishunkhe@arc-one.com>
2024-06-18 10:58:19 -0500

    fixed unit test and updated translation

--- ui/apps/distribution-ui/src/app/modules/shipment/view-shipping-label/view-shipping-label.component.spec.ts
+++ ui/apps/distribution-ui/src/app/modules/shipment/view-shipping-label/view-shipping-label.component.spec.ts
@@ -1,5 +1,10 @@
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ActivatedRoute } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { TranslateFakeLoader, TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { getAppInitializerMockProvider, RsaCommonsModule } from '@rsa/commons';
 import { ViewShippingLabelComponent } from './view-shipping-label.component';
 
 describe('ViewShippingLabelComponent', () => {
@@ -8,9 +13,26 @@ describe('ViewShippingLabelComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ViewShippingLabelComponent ]
-    })
-    .compileComponents();
+      declarations: [ViewShippingLabelComponent],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      imports: [
+        RsaCommonsModule,
+        BrowserAnimationsModule,
+        RouterTestingModule,
+        TranslateModule.forRoot({
+          loader: {
+            provide: TranslateLoader,
+            useClass: TranslateFakeLoader,
+          },
+        }),
+      ],
+      providers: [
+        ...getAppInitializerMockProvider('distribution-app'),
+        {
+          provide: ActivatedRoute,
+        },
+      ],
+    }).compileComponents();
   });
 
   beforeEach(() => {
