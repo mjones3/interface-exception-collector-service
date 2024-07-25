58c86f32fbf9e881faf42971d4ffa9e179e3538b
Michel Risucci <michel.risucci@arc-one.com>
2024-07-25 10:19:33 -0300

    [DIS-93] Added Local store for BioPro Orders

---
+++ backend/order/src/main/java/com/arcone/biopro/distribution/orderservice/adapter/in/web/dto/OrderDTO.java
@@ -0,0 +1,33 @@
+package com.arcone.biopro.distribution.orderservice.adapter.in.web.dto;
+
+import lombok.Builder;
+
+import java.time.LocalDate;
+import java.time.ZonedDateTime;
+import java.util.List;
+
+@Builder
+public record OrderDTO(
+    Long id,
+    Long orderNumber,
+    String externalId,
+    String locationCode,
+    String shipmentType,
+    String shippingMethod,
+    String shippingCustomerName,
+    String shippingCustomerCode,
+    String billingCustomerName,
+    String billingCustomerCode,
+    LocalDate desiredShippingDate,
+    Boolean willCallPickup,
+    String phoneNumber,
+    String productCategory,
+    String comments,
+    String status,
+    String priority,
+    String createEmployeeId,
+    ZonedDateTime createDate,
+    ZonedDateTime modificationDate,
+    ZonedDateTime deleteDate,
+    List<OrderItemDTO> orderItems
+) {}
