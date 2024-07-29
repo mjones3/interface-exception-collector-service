2b8428c9680724524cb5394f894458be2d3c585c
Lais Da Cunha <lais.dacunha@arc-one.com>
2024-07-29 22:08:05 +0000

    Husky Config

--- ui/src/app/modules/shipments/shipment-details/shipment-details.component.spec.ts
+++ ui/src/app/modules/shipments/shipment-details/shipment-details.component.spec.ts
@@ -13,82 +13,86 @@ import { ShipmentDetailsComponent } from './shipment-details.component';
 
 const SHIPMENT_ID = 1;
 describe('ShipmentDetailsComponent', () => {
-  let component: ShipmentDetailsComponent;
-  let fixture: ComponentFixture<ShipmentDetailsComponent>;
-  let shipmentService: ShipmentService;
-  let router: Router;
+    let component: ShipmentDetailsComponent;
+    let fixture: ComponentFixture<ShipmentDetailsComponent>;
+    let shipmentService: ShipmentService;
+    let router: Router;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [
-        ShipmentDetailsComponent, 
-        ApolloTestingModule,
-        NoopAnimationsModule,
-        ApolloModule,
-        TranslateModule.forRoot(),
-        ToastrModule.forRoot()
-      ],
-      providers: [
-        provideHttpClientTesting(),
-        provideMockStore({}),
-        {
-          provide: ActivatedRoute,
-          useValue: {
-            snapshot: {
-              params: {
-                id: SHIPMENT_ID,
-              },
-              data: {
-                shipmentDetailsConfigData: [],
-              }
-            }
-          }
-        }
-      ]
-    }).compileComponents();
-  });
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(ShipmentDetailsComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-    shipmentService = TestBed.inject(ShipmentService);
-    router = TestBed.inject(Router);
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-
-  it('should fetch shipment details', () => {
-    jest.spyOn(shipmentService, 'getShipmentById');
-    component.fetchShipmentDetails();
-    expect(shipmentService.getShipmentById).toHaveBeenCalledWith(SHIPMENT_ID, true);
-  });
-
-  it('should navigate back to search order', () => {
-    jest.spyOn(router, 'navigateByUrl');
-    component.backToSearch();
-    expect(router.navigateByUrl).toHaveBeenCalledWith('/orders/search');
-  });
-
-  it('should navigate to fill product when click on Fill Product button', () => {
-    const shipment = {
-      id: '1',
-    };
-    jest.spyOn(router, 'navigateByUrl');
-    component.fillProducts(shipment as ShipmentInfoItemDto);
-    expect(router.navigateByUrl).toHaveBeenCalledWith('shipment/1/fill-products/1');
-  });
-
-  it('should complete on click complete shipment button', () => {
-    component.loggedUserId = 'user-id-12';
-    jest.spyOn(shipmentService, 'completeShipment');
-    component.completeShipment();
-    expect(shipmentService.completeShipment).toHaveBeenCalledWith({
-      shipmentId: 1,
-      employeeId: 'user-id-12',
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            imports: [
+                ShipmentDetailsComponent,
+                ApolloTestingModule,
+                NoopAnimationsModule,
+                ApolloModule,
+                TranslateModule.forRoot(),
+                ToastrModule.forRoot(),
+            ],
+            providers: [
+                provideHttpClientTesting(),
+                provideMockStore({}),
+                {
+                    provide: ActivatedRoute,
+                    useValue: {
+                        snapshot: {
+                            params: {
+                                id: SHIPMENT_ID,
+                            },
+                            data: {
+                                shipmentDetailsConfigData: [],
+                            },
+                        },
+                    },
+                },
+            ],
+        }).compileComponents();
     });
-  });
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(ShipmentDetailsComponent);
+        component = fixture.componentInstance;
+        shipmentService = TestBed.inject(ShipmentService);
+        router = TestBed.inject(Router);
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('should fetch shipment details', () => {
+        jest.spyOn(shipmentService, 'getShipmentById');
+        component.fetchShipmentDetails();
+        expect(shipmentService.getShipmentById).toHaveBeenCalledWith(
+            SHIPMENT_ID,
+            true
+        );
+    });
+
+    it('should navigate back to search order', () => {
+        jest.spyOn(router, 'navigateByUrl');
+        component.backToSearch();
+        expect(router.navigateByUrl).toHaveBeenCalledWith('/orders/search');
+    });
+
+    it('should navigate to fill product when click on Fill Product button', () => {
+        const shipment = {
+            id: '1',
+        };
+        jest.spyOn(router, 'navigateByUrl');
+        component.fillProducts(shipment as ShipmentInfoItemDto);
+        expect(router.navigateByUrl).toHaveBeenCalledWith(
+            'shipment/1/fill-products/1'
+        );
+    });
+
+    it('should complete on click complete shipment button', () => {
+        component.loggedUserId = 'user-id-12';
+        jest.spyOn(shipmentService, 'completeShipment');
+        component.completeShipment();
+        expect(shipmentService.completeShipment).toHaveBeenCalledWith({
+            shipmentId: 1,
+            employeeId: 'user-id-12',
+        });
+    });
 });
--- ui/src/app/modules/shipments/shipment-details/shipment-details.component.ts
+++ ui/src/app/modules/shipments/shipment-details/shipment-details.component.ts
@@ -19,9 +19,9 @@ import { of, switchMap } from 'rxjs';
 import { catchError, take } from 'rxjs/operators';
 import { ERROR_MESSAGE } from '../../../../../public/i18n/common-labels';
 import {
-  DEFAULT_PAGE_SIZE,
-  DEFAULT_PAGE_SIZE_DIALOG_HEIGHT,
-  DEFAULT_PAGE_SIZE_DIALOG_WIDTH,
+    DEFAULT_PAGE_SIZE,
+    DEFAULT_PAGE_SIZE_DIALOG_HEIGHT,
+    DEFAULT_PAGE_SIZE_DIALOG_WIDTH,
 } from '../../../core/services/browser-printing/browser-printing.model';
 import { BrowserPrintingService } from '../../../core/services/browser-printing/browser-printing.service';
 import { getAuthState } from '../../../core/state/auth/auth.selectors';
@@ -32,10 +32,10 @@ import { ProcessProductDto } from '../../../shared/models/process-product.dto';
 import { ProcessHeaderService } from '../../../shared/services/process-header.service';
 import { SortService } from '../../../shared/services/sort.service';
 import {
-  FilledProductInfoDto,
-  ShipmentCompleteInfoDto,
-  ShipmentInfoDto,
-  ShipmentInfoItemDto,
+    FilledProductInfoDto,
+    ShipmentCompleteInfoDto,
+    ShipmentInfoDto,
+    ShipmentInfoItemDto,
 } from '../models/shipment-info.dto';
 import { PackingListService } from '../services/packing-list.service';
 import { ShipmentService } from '../services/shipment.service';
@@ -46,265 +46,295 @@ import { ViewPickListComponent } from '../view-pick-list/view-pick-list.componen
 import { ViewShippingLabelComponent } from '../view-shipping-label/view-shipping-label.component';
 
 @Component({
-  selector: 'app-shipment-details',
-  standalone: true,
-  imports: [
-    CommonModule,
-    TableModule,
-    MatDividerModule,
-    FuseCardComponent,
-    AsyncPipe,
-    ProcessHeaderComponent,
-    ToastrModule,
-    MatIconModule,
-    MatButtonModule,
-    MatProgressBarModule,
-    OrderWidgetsSidebarComponent,
-    ButtonModule,
-    DropdownModule
-  ],
-  templateUrl: './shipment-details.component.html',
-  styleUrl: './shipment-details.component.scss'
+    selector: 'app-shipment-details',
+    standalone: true,
+    imports: [
+        CommonModule,
+        TableModule,
+        MatDividerModule,
+        FuseCardComponent,
+        AsyncPipe,
+        ProcessHeaderComponent,
+        ToastrModule,
+        MatIconModule,
+        MatButtonModule,
+        MatProgressBarModule,
+        OrderWidgetsSidebarComponent,
+        ButtonModule,
+        DropdownModule,
+    ],
+    templateUrl: './shipment-details.component.html',
+    styleUrl: './shipment-details.component.scss',
 })
 export class ShipmentDetailsComponent implements OnInit {
-
-  constructor(
-    private _router: Router,
-    public header: ProcessHeaderService,
-    private route: ActivatedRoute,
-    private shipmentService: ShipmentService,
-    private toaster: ToastrService,
-    private sortService: SortService,
-    private packingListService: PackingListService,
-    private matDialog: MatDialog,
-    private store: Store,
-    private shippingLabelService: ShippingLabelService,
-    private browserPrintingService: BrowserPrintingService,
-    private translate: TranslateService,
-    @Inject(LOCALE_ID) public locale: string,
-  ) {
-    this.store
-      .select(getAuthState)
-      .pipe(take(1))
-      .subscribe(auth => {
-        this.loggedUserId = auth['id'];
-      });
-  }
-
-  expandedRows = {};
-  orderInfoDescriptions: Description[] = [];
-  shippingInfoDescriptions: Description[] = [];
-  shipmentInfo: ShipmentInfoDto;
-  products: ShipmentInfoItemDto[] = [];
-  processProductConfig: ProcessProductDto;
-  shippedInfoData: ShipmentCompleteInfoDto[] = [];
-  loggedUserId: string;
-  packedItems: FilledProductInfoDto[] = [];
-
-  get filledProductsCount() {
-    return this.packedItems?.length;
-  }
-
-  get isProductComplete(): boolean {
-    return this.shipmentInfo?.status === 'COMPLETED';
-  }
-
-  get labelingProductCategory() {
-    return this.shipmentInfo ? this.shipmentInfo?.productCategory : '';
-  }
-
-  get shipmentId() {
-    return this.route.snapshot.params?.id;
-  }
-
-  get totalProducts(): number {
-    return this.products.reduce<number>(
-      (previousValue: number, currentValue: ShipmentInfoItemDto) => previousValue + +currentValue?.quantity,
-      0
-    );
-  }
-
-  ngOnInit(): void {
-    this.fetchShipmentDetails();
-  }
-
-  fetchShipmentDetails(): void {
-    this.shipmentService.getShipmentById(this.shipmentId, true).subscribe(result => {
-      this.shipmentInfo = result.data?.getShipmentDetailsById;
-      this.products = this.shipmentInfo?.items?.map(item => this.convertItemToProduct(item)) ?? [];
-      this.getPackedItems();
-      this.updateWidgets();
-      if (this.isProductComplete) {
-        this.getShippedProductsInfo();
-      }
-    });
-  }
-
-  private convertItemToProduct(item: ShipmentInfoItemDto): ShipmentInfoItemDto {
-    return {
-      id: item.id,
-      quantity: item.quantity,
-      comments: item.comments,
-      productFamily: item.productFamily,
-      bloodType: item.bloodType,
-      packedItems: item.packedItems,
-    };
-  }
-
-  getShippedProductsInfo(): void {
-    this.shippedInfoData = [];
-    const details = {
-      completeDate: formatDate(this.shipmentInfo.completeDate, 'MM/dd/YYYY HH:mm', this.locale),
-      completedByEmployee: this.shipmentInfo.completedByEmployeeId,
-      quantity: this.packedItems?.length,
-    };
-    this.shippedInfoData.push(details);
-  }
-
-  getPackedItems(): void {
-    this.packedItems = [];
-    this.products.forEach(item => {
-      if (item.packedItems?.length) {
-        this.packedItems.push(...item.packedItems);
-      }
-    });
-  }
-
-  private updateWidgets() {
-    this.orderInfoDescriptions = this.shipmentService.getOrderInfoDescriptions(this.shipmentInfo);
-    this.shippingInfoDescriptions = this.shipmentService.getShippingInfoDescriptions(this.shipmentInfo);
-  }
-
-  fillProducts(item: ShipmentInfoItemDto): void {
-    const url = `shipment/${this.shipmentId}/fill-products/${item.id}`;
-    this._router.navigateByUrl(url);
-  }
-
-  getIcon(productFamily: string): string {
-    return productFamily && this.processProductConfig?.properties[`icon.${productFamily}`]
-      ? 'rsa:' + this.processProductConfig.properties[`icon.${productFamily}`]
-      : 'rsa:product-plasma';
-  }
-
-  backToSearch(): void {
-    this._router.navigateByUrl('/orders/search');
-  }
-
-  customSort(event: SortEvent) {
-    this.sortService.customSort(event);
-  }
-
-  viewPickList(): void {
-    const dialogRef = this.matDialog.open(ViewPickListComponent, {
-      id: 'ViewPickListDialog',
-      width: DEFAULT_PAGE_SIZE_DIALOG_WIDTH,
-      height: DEFAULT_PAGE_SIZE_DIALOG_HEIGHT,
-    });
-    dialogRef.componentInstance.model$ = of(this.shipmentInfo);
-  }
-
-  viewPackingList(print?: boolean): void {
-    let dialogRef: MatDialogRef<ViewPackingListComponent>;
-    this.packingListService
-      .generate(this.shipmentInfo.id)
-      .pipe(
-        switchMap(response => {
-          console.log('respose', response)
-          const packingListLabel = response?.data?.generatePackingListLabel;
-          dialogRef = this.matDialog.open(ViewPackingListComponent, {
-            id: 'ViewPackingListDialog',
-            ...(print
-              ? {
-                hasBackdrop: false,
-                panelClass: 'hidden',
-              }
-              : {
-                width: DEFAULT_PAGE_SIZE_DIALOG_WIDTH,
-                height: DEFAULT_PAGE_SIZE_DIALOG_HEIGHT,
-              }),
-          });
-          dialogRef.componentInstance.model$ = of(packingListLabel);
-          return dialogRef.afterOpened();
-        }),
-        catchError(err => {
-          this.toaster.error(ERROR_MESSAGE);
-          throw err;
-        })
-      )
-      .subscribe(() => {
-        this.browserPrintingService.print('viewPackingListReport', { pageSize: DEFAULT_PAGE_SIZE });
-        dialogRef?.close();
-      });
-  }
-
-  viewShippingLabel(print?: boolean): void {
-    let dialogRef: MatDialogRef<ViewShippingLabelComponent>;
-    this.shippingLabelService
-      .generate(this.shipmentInfo.id)
-      .pipe(
-        switchMap(response => {
-          const packingListLabel = response?.data?.generateShippingLabel;
-          dialogRef = this.matDialog.open(ViewShippingLabelComponent, {
-            id: 'ViewShippingLabelDialog',
-            ...(print
-              ? {
-                hasBackdrop: false,
-                panelClass: 'hidden',
-              }
-              : {
-                width: DEFAULT_PAGE_SIZE_DIALOG_WIDTH,
-                height: DEFAULT_PAGE_SIZE_DIALOG_HEIGHT,
-              }),
-          });
-          dialogRef.componentInstance.model$ = of(packingListLabel);
-          return dialogRef.afterOpened();
-        }),
-        catchError(err => {
-          this.toaster.error(ERROR_MESSAGE);
-          throw err;
-        })
-      )
-      .subscribe(() => {
-        this.browserPrintingService.print('viewShippingLabelReport', { pageSize: DEFAULT_PAGE_SIZE });
-        dialogRef?.close();
-      });
-  }
-
-  completeShipment() {
-    this.shipmentService.completeShipment(this.getValidateRuleDto()).subscribe({
-      next: (response) => {
-        const value = response.data?.completeShipment;
-        const notifications = value.notifications;
-        const url = value._links?.next;
-
-        if (notifications?.length) {
-          this.displayMessageFromNotificationDto(notifications[0]);
-          if (url && notifications[0].notificationType === 'success') {
-            this.fetchShipmentDetails();
-          }
-        }
-      },
-      error: (err) => {
-        this.toaster.error(ERROR_MESSAGE);
-        throw err;
-      }
+    constructor(
+        private _router: Router,
+        public header: ProcessHeaderService,
+        private route: ActivatedRoute,
+        private shipmentService: ShipmentService,
+        private toaster: ToastrService,
+        private sortService: SortService,
+        private packingListService: PackingListService,
+        private matDialog: MatDialog,
+        private store: Store,
+        private shippingLabelService: ShippingLabelService,
+        private browserPrintingService: BrowserPrintingService,
+        private translate: TranslateService,
+        @Inject(LOCALE_ID) public locale: string
+    ) {
+        this.store
+            .select(getAuthState)
+            .pipe(take(1))
+            .subscribe((auth) => {
+                this.loggedUserId = auth['id'];
+            });
     }
-    );
-  }
 
-  private getValidateRuleDto() {
-    return {
-      shipmentId: this.shipmentId,
-      employeeId: this.loggedUserId,
-    };
-  }
+    expandedRows = {};
+    orderInfoDescriptions: Description[] = [];
+    shippingInfoDescriptions: Description[] = [];
+    shipmentInfo: ShipmentInfoDto;
+    products: ShipmentInfoItemDto[] = [];
+    processProductConfig: ProcessProductDto;
+    shippedInfoData: ShipmentCompleteInfoDto[] = [];
+    loggedUserId: string;
+    packedItems: FilledProductInfoDto[] = [];
 
-  displayMessageFromNotificationDto(notification: NotificationDto) {
-    this.toaster.show(
-      this.translate.instant(notification.message),
-      startCase(notification.notificationType),
-      {},
-      notification.notificationType
-    );
-  }
+    get filledProductsCount() {
+        return this.packedItems?.length;
+    }
+
+    get isProductComplete(): boolean {
+        return this.shipmentInfo?.status === 'COMPLETED';
+    }
+
+    get labelingProductCategory() {
+        return this.shipmentInfo ? this.shipmentInfo?.productCategory : '';
+    }
+
+    get shipmentId() {
+        return this.route.snapshot.params?.id;
+    }
+
+    get totalProducts(): number {
+        return this.products.reduce<number>(
+            (previousValue: number, currentValue: ShipmentInfoItemDto) =>
+                previousValue + +currentValue?.quantity,
+            0
+        );
+    }
+
+    ngOnInit(): void {
+        this.fetchShipmentDetails();
+    }
+
+    fetchShipmentDetails(): void {
+        this.shipmentService
+            .getShipmentById(this.shipmentId, true)
+            .subscribe((result) => {
+                this.shipmentInfo = result.data?.getShipmentDetailsById;
+                this.products =
+                    this.shipmentInfo?.items?.map((item) =>
+                        this.convertItemToProduct(item)
+                    ) ?? [];
+                this.getPackedItems();
+                this.updateWidgets();
+                if (this.isProductComplete) {
+                    this.getShippedProductsInfo();
+                }
+            });
+    }
+
+    private convertItemToProduct(
+        item: ShipmentInfoItemDto
+    ): ShipmentInfoItemDto {
+        return {
+            id: item.id,
+            quantity: item.quantity,
+            comments: item.comments,
+            productFamily: item.productFamily,
+            bloodType: item.bloodType,
+            packedItems: item.packedItems,
+        };
+    }
+
+    getShippedProductsInfo(): void {
+        this.shippedInfoData = [];
+        const details = {
+            completeDate: formatDate(
+                this.shipmentInfo.completeDate,
+                'MM/dd/YYYY HH:mm',
+                this.locale
+            ),
+            completedByEmployee: this.shipmentInfo.completedByEmployeeId,
+            quantity: this.packedItems?.length,
+        };
+        this.shippedInfoData.push(details);
+    }
+
+    getPackedItems(): void {
+        this.packedItems = [];
+        this.products.forEach((item) => {
+            if (item.packedItems?.length) {
+                this.packedItems.push(...item.packedItems);
+            }
+        });
+    }
+
+    private updateWidgets() {
+        this.orderInfoDescriptions =
+            this.shipmentService.getOrderInfoDescriptions(this.shipmentInfo);
+        this.shippingInfoDescriptions =
+            this.shipmentService.getShippingInfoDescriptions(this.shipmentInfo);
+    }
+
+    fillProducts(item: ShipmentInfoItemDto): void {
+        const url = `shipment/${this.shipmentId}/fill-products/${item.id}`;
+        this._router.navigateByUrl(url);
+    }
+
+    getIcon(productFamily: string): string {
+        return productFamily &&
+            this.processProductConfig?.properties[`icon.${productFamily}`]
+            ? 'rsa:' +
+                  this.processProductConfig.properties[`icon.${productFamily}`]
+            : 'rsa:product-plasma';
+    }
+
+    backToSearch(): void {
+        this._router.navigateByUrl('/orders/search');
+    }
+
+    customSort(event: SortEvent) {
+        this.sortService.customSort(event);
+    }
+
+    viewPickList(): void {
+        const dialogRef = this.matDialog.open(ViewPickListComponent, {
+            id: 'ViewPickListDialog',
+            width: DEFAULT_PAGE_SIZE_DIALOG_WIDTH,
+            height: DEFAULT_PAGE_SIZE_DIALOG_HEIGHT,
+        });
+        dialogRef.componentInstance.model$ = of(this.shipmentInfo);
+    }
+
+    viewPackingList(print?: boolean): void {
+        let dialogRef: MatDialogRef<ViewPackingListComponent>;
+        this.packingListService
+            .generate(this.shipmentInfo.id)
+            .pipe(
+                switchMap((response) => {
+                    console.log('respose', response);
+                    const packingListLabel =
+                        response?.data?.generatePackingListLabel;
+                    dialogRef = this.matDialog.open(ViewPackingListComponent, {
+                        id: 'ViewPackingListDialog',
+                        ...(print
+                            ? {
+                                  hasBackdrop: false,
+                                  panelClass: 'hidden',
+                              }
+                            : {
+                                  width: DEFAULT_PAGE_SIZE_DIALOG_WIDTH,
+                                  height: DEFAULT_PAGE_SIZE_DIALOG_HEIGHT,
+                              }),
+                    });
+                    dialogRef.componentInstance.model$ = of(packingListLabel);
+                    return dialogRef.afterOpened();
+                }),
+                catchError((err) => {
+                    this.toaster.error(ERROR_MESSAGE);
+                    throw err;
+                })
+            )
+            .subscribe(() => {
+                this.browserPrintingService.print('viewPackingListReport', {
+                    pageSize: DEFAULT_PAGE_SIZE,
+                });
+                dialogRef?.close();
+            });
+    }
+
+    viewShippingLabel(print?: boolean): void {
+        let dialogRef: MatDialogRef<ViewShippingLabelComponent>;
+        this.shippingLabelService
+            .generate(this.shipmentInfo.id)
+            .pipe(
+                switchMap((response) => {
+                    const packingListLabel =
+                        response?.data?.generateShippingLabel;
+                    dialogRef = this.matDialog.open(
+                        ViewShippingLabelComponent,
+                        {
+                            id: 'ViewShippingLabelDialog',
+                            ...(print
+                                ? {
+                                      hasBackdrop: false,
+                                      panelClass: 'hidden',
+                                  }
+                                : {
+                                      width: DEFAULT_PAGE_SIZE_DIALOG_WIDTH,
+                                      height: DEFAULT_PAGE_SIZE_DIALOG_HEIGHT,
+                                  }),
+                        }
+                    );
+                    dialogRef.componentInstance.model$ = of(packingListLabel);
+                    return dialogRef.afterOpened();
+                }),
+                catchError((err) => {
+                    this.toaster.error(ERROR_MESSAGE);
+                    throw err;
+                })
+            )
+            .subscribe(() => {
+                this.browserPrintingService.print('viewShippingLabelReport', {
+                    pageSize: DEFAULT_PAGE_SIZE,
+                });
+                dialogRef?.close();
+            });
+    }
+
+    completeShipment() {
+        this.shipmentService
+            .completeShipment(this.getValidateRuleDto())
+            .subscribe({
+                next: (response) => {
+                    const value = response.data?.completeShipment;
+                    const notifications = value.notifications;
+                    const url = value._links?.next;
+
+                    if (notifications?.length) {
+                        this.displayMessageFromNotificationDto(
+                            notifications[0]
+                        );
+                        if (
+                            url &&
+                            notifications[0].notificationType === 'success'
+                        ) {
+                            this.fetchShipmentDetails();
+                        }
+                    }
+                },
+                error: (err) => {
+                    this.toaster.error(ERROR_MESSAGE);
+                    throw err;
+                },
+            });
+    }
+
+    private getValidateRuleDto() {
+        return {
+            shipmentId: this.shipmentId,
+            employeeId: this.loggedUserId,
+        };
+    }
+
+    displayMessageFromNotificationDto(notification: NotificationDto) {
+        this.toaster.show(
+            this.translate.instant(notification.message),
+            startCase(notification.notificationType),
+            {},
+            notification.notificationType
+        );
+    }
 }
