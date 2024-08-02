967e6fcc370f18ea410468103832b2e448733fbf
allanbraga <Allan.MorelliBraga@arc-one.com>
2024-08-02 16:45:21 -0300

    feat(DIS-95): list of all orders in Search Order

---
+++ backend/order/src/main/java/com/arcone/biopro/distribution/order/application/mapper/OrderQueryMapper.java
@@ -0,0 +1,24 @@
+package com.arcone.biopro.distribution.order.application.mapper;
+
+import com.arcone.biopro.distribution.order.adapter.in.web.dto.OrderQueryCommandDTO;
+import com.arcone.biopro.distribution.order.domain.model.OrderQueryCommand;
+import com.arcone.biopro.distribution.order.domain.model.QueryOrderBy;
+import com.arcone.biopro.distribution.order.domain.model.QuerySort;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+@RequiredArgsConstructor
+public class OrderQueryMapper {
+
+    public OrderQueryCommand mapToDomain(final OrderQueryCommandDTO commandDTO) {
+        return new OrderQueryCommand(commandDTO.locationCode(),
+            Optional.ofNullable(commandDTO.querySort()).map(querySortDTO -> new QuerySort(Optional
+                .of(querySortDTO.orderByList().stream().map(sortDto -> new QueryOrderBy(sortDto.property(),sortDto.direction())).toList())
+                .orElse(null))).orElse(null),
+            commandDTO.limit());
+    }
+
+}
---
+++ backend/order/src/test/java/com/arcone/biopro/distribution/order/unit/domain/model/OrderQueryCommandTest.java
@@ -0,0 +1,47 @@
+package com.arcone.biopro.distribution.order.unit.domain.model;
+
+import com.arcone.biopro.distribution.order.domain.model.OrderQueryCommand;
+import com.arcone.biopro.distribution.order.domain.model.QueryOrderBy;
+import com.arcone.biopro.distribution.order.domain.model.QuerySort;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class OrderQueryCommandTest {
+
+    @Test
+    public void shouldCreateOrderQueryCommandWhenSortIsNull() {
+        var orderQueryCommand = new OrderQueryCommand("1",null,10);
+        Assertions.assertNotNull(orderQueryCommand);
+        Assertions.assertNotNull(orderQueryCommand.getQuerySort());
+        Assertions.assertNotNull(orderQueryCommand.getQuerySort().getQueryOrderByList());
+        Assertions.assertEquals("ASC",orderQueryCommand.getQuerySort().getQueryOrderByList().get(0).getDirection());
+        Assertions.assertEquals("priority",orderQueryCommand.getQuerySort().getQueryOrderByList().get(0).getProperty());
+    }
+
+    @Test
+    public void shouldCreateOrderQueryCommand() {
+        var orderBy = new QueryOrderBy("TEST","DESC");
+        var sort = new QuerySort(List.of(orderBy));
+        var orderQueryCommand = new OrderQueryCommand("1",sort,10);
+        Assertions.assertNotNull(orderQueryCommand);
+        Assertions.assertNotNull(orderQueryCommand.getQuerySort());
+        Assertions.assertNotNull(orderQueryCommand.getQuerySort().getQueryOrderByList());
+        Assertions.assertEquals("DESC",orderQueryCommand.getQuerySort().getQueryOrderByList().get(0).getDirection());
+        Assertions.assertEquals("TEST",orderQueryCommand.getQuerySort().getQueryOrderByList().get(0).getProperty());
+    }
+
+    @Test
+    public void shouldNotCreateOrderQueryCommand() {
+        assertThrows(IllegalArgumentException.class, () -> new OrderQueryCommand(null,null,null));
+
+        Exception exception =  assertThrows(IllegalArgumentException.class, () -> new OrderQueryCommand(null,null,10));
+        Assertions.assertEquals("locationCode cannot be null or empty", exception.getMessage());
+
+        exception =  assertThrows(IllegalArgumentException.class, () -> new OrderQueryCommand("TEST",null,-1));
+        Assertions.assertEquals("limit must be greater than 0", exception.getMessage());
+    }
+}
