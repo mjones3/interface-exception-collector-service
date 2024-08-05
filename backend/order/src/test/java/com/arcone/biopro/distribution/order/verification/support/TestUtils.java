482bdcd50f9126e614bc893e4e6e38be780b58c9
Benjamin Pinto <benjamin.pinto@arc-one.com>
2024-08-05 14:40:14 -0400

    feat(dis-95): configuration for selenium and few steps definitions

--- backend/order/src/test/java/com/arcone/biopro/distribution/order/verification/support/TestUtils.java
+++ backend/order/src/test/java/com/arcone/biopro/distribution/order/verification/support/TestUtils.java
@@ -6,6 +6,7 @@ import org.testcontainers.shaded.org.apache.commons.io.FileUtils;
 
 import java.io.File;
 import java.net.URL;
+import java.util.Arrays;
 
 @Component
 @Slf4j
@@ -15,7 +16,12 @@ public class TestUtils {
         ClassLoader classLoader = TestUtils.class.getClassLoader();
         URL resource = classLoader.getResource(fileName);
         return FileUtils.readFileToString(new File(resource.toURI()));
+    }
 
+    public String formatSqlCommaSeparatedInParamList(String param) {
+        var paramList = Arrays.stream(param.split(",")).map(String::trim).toArray(String[]::new);
+        var formattedParam = String.join("','", paramList);
+        return String.format("('%s')", formattedParam);
     }
 
 }
