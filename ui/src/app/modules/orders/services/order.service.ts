31c79818bcabefbf5c011069037b3161970d591f
Michel Risucci <michel.risucci@arc-one.com>
2024-08-05 19:31:14 -0300

    feat: added Search Order table page

--- ui/src/app/modules/orders/services/order.service.ts
+++ ui/src/app/modules/orders/services/order.service.ts
@@ -4,6 +4,8 @@ import { Apollo } from 'apollo-angular';
 import { LIST_SHIPMENT } from 'app/modules/shipments/graphql/shipment/query-definitions/shipment.graphql';
 import { Observable } from 'rxjs';
 import { OrderSummaryDto } from '../models/order.dto';
+import { OrderQueryCommandDTO, OrderReportDTO } from '../order/models/search-order.model';
+import { SEARCH_ORDERS } from '../../shipments/graphql/order/query-definitions/search-orders.graphql';
 
 @Injectable({
     providedIn: 'root',
@@ -24,4 +26,16 @@ export class OrderService {
             ...(refetch ? { fetchPolicy: 'network-only' } : {}),
         });
     }
+
+    public searchOrders(commandQuery: OrderQueryCommandDTO, refetch = false
+    ) : Observable<ApolloQueryResult<{ searchOrders: OrderReportDTO[] }>> {
+        return this.apolloClient.query<{ searchOrders: OrderReportDTO[] }>({
+            query: SEARCH_ORDERS,
+            variables: {
+                orderQueryCommandDTO: commandQuery,
+            },
+            ...(refetch ? { fetchPolicy: 'network-only' } : {}),
+        });
+    }
+
 }
