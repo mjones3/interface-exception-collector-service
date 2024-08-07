9048b69eef8623c32842cb5d58d0eaee47f0e07e
Michel Risucci <michel.risucci@arc-one.com>
2024-08-07 10:34:38 -0300

    [DIS-95] Added single lookup database query to map priorities

--- backend/order/src/main/java/com/arcone/biopro/distribution/order/infrastructure/persistence/OrderQueryRepositoryImpl.java
+++ backend/order/src/main/java/com/arcone/biopro/distribution/order/infrastructure/persistence/OrderQueryRepositoryImpl.java
@@ -7,46 +7,65 @@ import com.arcone.biopro.distribution.order.infrastructure.mapper.OrderReportEnt
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.r2dbc.core.R2dbcEntityTemplate;
-import org.springframework.data.relational.core.query.Query;
+import org.springframework.data.relational.core.query.Criteria;
 import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+import static java.lang.Boolean.TRUE;
 import static org.springframework.data.relational.core.query.Criteria.where;
+import static org.springframework.data.relational.core.query.Query.query;
 
 @Repository
 @RequiredArgsConstructor
 public class OrderQueryRepositoryImpl implements OrderQueryRepository {
 
+    private static final String ORDER_PRIORITY_COLOR_TYPE = "ORDER_PRIORITY_COLOR";
+
     private final R2dbcEntityTemplate entityTemplate;
     private final OrderReportEntityMapper orderReportEntityMapper;
-    private static final String ORDER_PRIORITY_COLOR_TYPE = "ORDER_PRIORITY_COLOR";
 
     @Override
     public Flux<OrderReport> searchOrders(OrderQueryCommand orderQueryCommand) {
         var criteria = where("locationCode").is(orderQueryCommand.getLocationCode());
-        var sort = orderQueryCommand.getQuerySort()
+        var sorts = orderQueryCommand.getQuerySort()
             .getQueryOrderByList().stream()
             .map(orderBy -> new Sort.Order(Sort.Direction.fromString(orderBy.getDirection()), orderBy.getProperty()))
             .toList();
 
+        var orders = queryOrdersByCriteria(criteria, Sort.by(sorts), orderQueryCommand.getLimit());
+        var priorityMap = queryLookupsByTypeToMapByOrderNumber(ORDER_PRIORITY_COLOR_TYPE);
+        return Flux.zip(orders, priorityMap.repeat())
+            .map(tuple -> {
+                var orderReport = tuple.getT1();
+                var orderPriorityLookupColorMap = tuple.getT2();
+                var colorLookupEntry = orderPriorityLookupColorMap.get(orderReport.getPriority());
+                return orderReportEntityMapper.mapToDomain(orderReport, colorLookupEntry.getOptionValue());
+            });
+    }
+
+    private Flux<OrderEntity> queryOrdersByCriteria(Criteria criteria, Sort sort, int limit) {
         return entityTemplate.select(OrderEntity.class)
-                    .matching(Query.query(criteria)
-                        .sort(Sort.by(sort))
-                        .limit(orderQueryCommand.getLimit())
-                    ).all().flatMapSequential(orderEntity -> findPriorityColor(orderEntity.getDeliveryType())
-                .map(lookup -> orderReportEntityMapper.mapToDomain(orderEntity,lookup.getOptionValue()))
-            );
-
-
+            .matching(
+                query(criteria)
+                    .sort(sort)
+                    .limit(limit)
+            )
+            .all();
     }
 
-    private Mono<LookupEntity> findPriorityColor(final String priority){
-        return this.entityTemplate.select(LookupEntity.class)
-            .matching(Query.query(
-                where("type").is(ORDER_PRIORITY_COLOR_TYPE)
-                    .and("active").is(Boolean.TRUE)
-                    .and("descriptionKey").is(priority)
-            )).one();
+    private Mono<Map<Integer, LookupEntity>> queryLookupsByTypeToMapByOrderNumber(String type) {
+        return entityTemplate.select(LookupEntity.class)
+            .matching(query(
+                where("type").is(type)
+                    .and("active").is(TRUE)
+            ))
+            .all()
+            .collect(Collectors.toMap(LookupEntity::getOrderNumber, Function.identity()));
     }
+
 }
