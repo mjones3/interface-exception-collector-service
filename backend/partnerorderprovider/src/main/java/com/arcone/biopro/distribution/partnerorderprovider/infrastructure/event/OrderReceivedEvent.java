2ac72e1dd72331da354f3fcdc300c4f54d7731ab
allanbraga <Allan.MorelliBraga@arc-one.com>
2024-08-19 15:16:15 -0300

    feat(DIS-129): integrate UI to all back-end services - partner order providers update endpoints

--- backend/partnerorderprovider/src/main/java/com/arcone/biopro/distribution/partnerorderprovider/infrastructure/event/OrderReceivedEvent.java
+++ backend/partnerorderprovider/src/main/java/com/arcone/biopro/distribution/partnerorderprovider/infrastructure/event/OrderReceivedEvent.java
@@ -1,11 +1,15 @@
 package com.arcone.biopro.distribution.partnerorderprovider.infrastructure.event;
 
 import com.arcone.biopro.distribution.partnerorderprovider.infrastructure.listener.dto.OrderDTO;
+import lombok.Builder;
 
 import java.io.Serializable;
 
+@Builder
 public record OrderReceivedEvent(
-    OrderDTO orderDTO
+    String eventType,
+    String eventVersion,
+    OrderDTO payload
 ) implements Serializable {
 
 }
