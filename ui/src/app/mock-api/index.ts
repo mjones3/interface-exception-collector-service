252ac9096851b509d78a027be8dc0087d6f12b7d
allanbraga <Allan.MorelliBraga@arc-one.com>
2024-09-11 18:48:44 -0300

    feat(DIS-152): product Validation - Trigger Discard for Unacceptable Products - UI Services - back-end refactor

--- ui/src/app/mock-api/index.ts
+++ ui/src/app/mock-api/index.ts
@@ -1,4 +1,9 @@
 import { NavigationMockApi } from 'app/mock-api/common/navigation/api';
+import { DiscardMockApi } from './common/discard/api';
 import { ProcessMockApi } from './common/process/api';
 
-export const mockApiServices = [NavigationMockApi, ProcessMockApi];
+export const mockApiServices = [
+    NavigationMockApi,
+    ProcessMockApi,
+    DiscardMockApi,
+];
--- ui/src/app/shared/services/discard.service.ts
+++ ui/src/app/shared/services/discard.service.ts
@@ -1,23 +1,25 @@
 import { Injectable } from '@angular/core';
+import { MutationResult } from 'apollo-angular';
+import { Observable } from 'rxjs';
 import { DynamicGraphqlPathService } from '../../core/services/dynamic-graphql-path.service';
 import { ADD_DISCARD } from '../graphql/discard/mutation-definitions/discard.graphql';
-import { Observable } from 'rxjs';
-import { MutationResult } from 'apollo-angular';
 import { DiscardRequestDTO, DiscardResponseDTO } from '../models/discard.model';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root',
 })
 export class DiscardService {
-
     readonly servicePath = '/discard/graphql';
 
     constructor(private dynamicGraphqlPathService: DynamicGraphqlPathService) {}
 
-    public discardProduct(inputs: { discard: DiscardRequestDTO})
-        : Observable<MutationResult<{ discardProduct: DiscardResponseDTO }>> {
-        return this.dynamicGraphqlPathService
-            .executeMutation(this.servicePath, ADD_DISCARD, inputs);
+    public discardProduct(
+        discardRequest: DiscardRequestDTO
+    ): Observable<MutationResult<{ discardProduct: DiscardResponseDTO }>> {
+        return this.dynamicGraphqlPathService.executeMutation(
+            this.servicePath,
+            ADD_DISCARD,
+            { discardRequest }
+        );
     }
-
 }
