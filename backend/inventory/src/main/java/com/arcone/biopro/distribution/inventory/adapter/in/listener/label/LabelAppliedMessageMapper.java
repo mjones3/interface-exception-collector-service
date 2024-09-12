80b49091e00cb2d84419e6d338244e1da47dba85
Anderson Paulino <anderson.paulino@arc-one.com>
2024-09-12 21:46:01 +0000

    LAB-148: change to productDescription in message

--- backend/inventory/src/main/java/com/arcone/biopro/distribution/inventory/adapter/in/listener/label/LabelAppliedMessageMapper.java
+++ backend/inventory/src/main/java/com/arcone/biopro/distribution/inventory/adapter/in/listener/label/LabelAppliedMessageMapper.java
@@ -2,9 +2,11 @@ package com.arcone.biopro.distribution.inventory.adapter.in.listener.label;
 
 import com.arcone.biopro.distribution.inventory.application.dto.InventoryInput;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface LabelAppliedMessageMapper {
 
+    @Mapping(target = "shortDescription", source = "productDescription")
     InventoryInput toInput(LabelAppliedMessage message);
 }
