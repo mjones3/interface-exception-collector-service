d064d8f99431003477bcc9ce8eda1c43bb66017b
Michel Risucci <michel.risucci@arc-one.com>
2024-10-31 11:33:04 -0300

    feat(DIS-203): added widget to verify products page

--- ui/src/app/modules/shipments/verify-products/verify-products.component.spec.ts
+++ ui/src/app/modules/shipments/verify-products/verify-products.component.spec.ts
@@ -1,18 +1,36 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { provideHttpClientTesting } from '@angular/common/http/testing';
-import { ActivatedRoute } from '@angular/router';
+import { MatIconTestingModule } from '@angular/material/icon/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { ActivatedRoute, Router } from '@angular/router';
 import { provideMockStore } from '@ngrx/store/testing';
+import { TranslateModule } from '@ngx-translate/core';
+import { ApolloModule } from 'apollo-angular';
+import { ApolloTestingModule } from 'apollo-angular/testing';
+import { ToastrModule } from 'ngx-toastr';
+import { of } from 'rxjs';
+import { ShipmentService } from '../services/shipment.service';
 import { VerifyProductsComponent } from './verify-products.component';
 
 const SHIPMENT_ID = 1;
 describe('VerifyProductsComponent', () => {
     let component: VerifyProductsComponent;
     let fixture: ComponentFixture<VerifyProductsComponent>;
+    let shipmentService: ShipmentService;
+    let router: Router;
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
-            imports: [VerifyProductsComponent],
+            imports: [
+                VerifyProductsComponent,
+                ApolloTestingModule,
+                NoopAnimationsModule,
+                MatIconTestingModule,
+                ApolloModule,
+                TranslateModule.forRoot(),
+                ToastrModule.forRoot(),
+            ],
             providers: [
                 provideHttpClientTesting(),
                 provideMockStore({}),
@@ -26,11 +44,15 @@ describe('VerifyProductsComponent', () => {
                         },
                     },
                 },
+                ShipmentService,
             ],
         }).compileComponents();
 
         fixture = TestBed.createComponent(VerifyProductsComponent);
         component = fixture.componentInstance;
+        shipmentService = TestBed.inject(ShipmentService);
+        router = TestBed.inject(Router);
+        jest.spyOn(shipmentService, 'getShipmentById').mockReturnValue(of());
         fixture.detectChanges();
     });
 
--- ui/src/app/modules/shipments/verify-products/verify-products.component.ts
+++ ui/src/app/modules/shipments/verify-products/verify-products.component.ts
@@ -1,19 +1,58 @@
-import { AsyncPipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { AsyncPipe, NgTemplateOutlet, PercentPipe } from '@angular/common';
+import { Component, OnInit, computed, signal } from '@angular/core';
+import { MatProgressBar } from '@angular/material/progress-bar';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ProcessHeaderComponent, ProcessHeaderService } from '@shared';
+import { FuseCardComponent } from '../../../../@fuse';
 import { ActionButtonComponent } from '../../../shared/components/action-button/action-button.component';
+import {
+    ShipmentDetailResponseDTO,
+    ShipmentItemResponseDTO,
+} from '../models/shipment-info.dto';
+import { ShipmentService } from '../services/shipment.service';
+import { OrderWidgetsSidebarComponent } from '../shared/order-widgets-sidebar/order-widgets-sidebar.component';
 
 @Component({
     selector: 'app-verify-products',
     standalone: true,
-    imports: [AsyncPipe, ProcessHeaderComponent, ActionButtonComponent],
+    imports: [
+        AsyncPipe,
+        ProcessHeaderComponent,
+        ActionButtonComponent,
+        FuseCardComponent,
+        MatProgressBar,
+        NgTemplateOutlet,
+        OrderWidgetsSidebarComponent,
+        PercentPipe,
+    ],
     templateUrl: './verify-products.component.html',
 })
-export class VerifyProductsComponent {
+export class VerifyProductsComponent implements OnInit {
+    protected shipmentId: number;
+
+    protected shipmentSignal = signal<ShipmentDetailResponseDTO>(null);
+    protected productItems = computed<ShipmentItemResponseDTO[]>(() =>
+        this.shipmentSignal()?.items ? [...this.shipmentSignal().items] : []
+    );
+
     constructor(
         private route: ActivatedRoute,
         private router: Router,
+        private shipmentService: ShipmentService,
         protected header: ProcessHeaderService
-    ) {}
+    ) {
+        this.shipmentId = Number(this.route.snapshot.params?.id);
+    }
+
+    ngOnInit(): void {
+        this.fetchShipmentDetails();
+    }
+
+    fetchShipmentDetails(): void {
+        this.shipmentService
+            .getShipmentById(this.shipmentId)
+            .subscribe((result) => {
+                this.shipmentSignal.set(result.data?.getShipmentDetailsById);
+            });
+    }
 }
