6508d83659ebdc6103c869b15fc5b1f3bade3c85
Ram <ram.bishunkhe@arc-one.com>
2024-11-01 10:50:46 -0500

    feat(dis-201): progress bar

--- ui/src/app/progress-bar/progress-bar.component.spec.ts
+++ ui/src/app/progress-bar/progress-bar.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { ProgressBarComponent } from './progress-bar.component';
 
 describe('ProgressBarComponent', () => {
@@ -16,5 +17,27 @@ describe('ProgressBarComponent', () => {
 
     it('should create', () => {
         expect(component).toBeTruthy();
+    });
+
+    it('should display progress bar buffer as complete when value is 100', () => {
+        component.value = 100;
+        fixture.detectChanges();
+        const progressBar = fixture.debugElement.query(
+            By.css('mat-progress-bar')
+        );
+        expect(component.value).toBe(100);
+        expect(progressBar.classes['inProgress']).toBeFalsy();
+        expect(progressBar.classes['complete']).toBeTruthy();
+    });
+
+    it('should display progress bar buffer as inProgress when value is greater than 0 and less than 100', () => {
+        component.value = 60;
+        fixture.detectChanges();
+        const progressBar = fixture.debugElement.query(
+            By.css('mat-progress-bar')
+        );
+        expect(component.value).toBe(60);
+        expect(progressBar.classes['inProgress']).toBeTruthy();
+        expect(progressBar.classes['complete']).toBeFalsy();
     });
 });
