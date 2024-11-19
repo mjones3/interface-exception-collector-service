85f257e56269f69b647619edcc413fce164b47e9
Michel Risucci <michel.risucci@arc-one.com>
2024-11-19 10:46:51 -0300

    feat(DIS-207): updated unit-number-card to hold ineligible status badge

--- ui/src/app/modules/shipments/verify-products/verify-products.component.ts
+++ ui/src/app/modules/shipments/verify-products/verify-products.component.ts
@@ -1,6 +1,7 @@
 import { AsyncPipe, NgTemplateOutlet, PercentPipe } from '@angular/common';
 import { Component, OnInit, ViewChild, computed, signal } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { AsyncPipe, PercentPipe } from '@angular/common';
 import { MatDivider } from '@angular/material/divider';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Store } from '@ngrx/store';
@@ -15,22 +16,20 @@ import { ScanUnitNumberProductCodeComponent } from 'app/scan-unit-number-product
 import { NotificationComponent } from 'app/shared/components/notification/notification.component';
 import { finalize, forkJoin, take, tap } from 'rxjs';
 import { catchError } from 'rxjs/operators';
+import { ToastrService } from 'ngx-toastr';
+import { finalize, tap } from 'rxjs';
 import { FuseCardComponent } from '../../../../@fuse';
-import { getAuthState } from '../../../core/state/auth/auth.selectors';
 import { ProgressBarComponent } from '../../../progress-bar/progress-bar.component';
 import { ActionButtonComponent } from '../../../shared/components/action-button/action-button.component';
+import { GlobalMessageComponent } from '../../../shared/components/global-message/global-message.component';
 import { UnitNumberCardComponent } from '../../../shared/components/unit-number-card/unit-number-card.component';
 import { ProductIconsService } from '../../../shared/services/product-icon.service';
-import handleApolloError from '../../../shared/utils/apollo-error-handling';
 import { consumeNotifications } from '../../../shared/utils/notification.handling';
-import { VerifyProductResponseDTO } from '../graphql/verify-products/query-definitions/verify-products.graphql';
-import {
-    ShipmentDetailResponseDTO,
-    ShipmentItemPackedDTO,
-    VerifyFilledProductDto,
-} from '../models/shipment-info.dto';
+import { VerifyFilledProductDto } from '../models/shipment-info.dto';
+import { SecondVerificationCommon } from '../second-verification-common';
 import { ShipmentService } from '../services/shipment.service';
 import { OrderWidgetsSidebarComponent } from '../shared/order-widgets-sidebar/order-widgets-sidebar.component';
+import { VerifyProductsNavbarComponent } from '../verify-products-navbar/verify-products-navbar.component';
 
 @Component({
     selector: 'app-verify-products',
@@ -40,33 +39,25 @@ import { OrderWidgetsSidebarComponent } from '../shared/order-widgets-sidebar/or
         ProcessHeaderComponent,
         ActionButtonComponent,
         FuseCardComponent,
-        NgTemplateOutlet,
         OrderWidgetsSidebarComponent,
         PercentPipe,
         MatDivider,
         UnitNumberCardComponent,
         ProgressBarComponent,
         ScanUnitNumberProductCodeComponent,
+        VerifyProductsNavbarComponent,
+        GlobalMessageComponent,
         NotificationComponent,
     ],
     templateUrl: './verify-products.component.html',
 })
-export class VerifyProductsComponent implements OnInit {
-    protected loggedUserIdSignal = signal<string>(null);
-    protected shipmentSignal = signal<ShipmentDetailResponseDTO>(null);
-    protected verificationSignal = signal<VerifyProductResponseDTO>(null);
-    protected packedItemsComputed = computed(
-        () => this.verificationSignal()?.packedItems ?? []
-    );
-    protected verifiedItemsComputed = computed(
-        () => this.verificationSignal()?.verifiedItems ?? []
-    );
-    protected isAllPackItemsVerified = computed(
+export class VerifyProductsComponent
+    extends SecondVerificationCommon
+    implements OnInit
+{
+    protected verifyProductsNotificationsRouteComputed = computed(
         () =>
-            this.packedItemsComputed()?.length &&
-            this.verifiedItemsComputed()?.length &&
-            this.packedItemsComputed().length ===
-                this.verifiedItemsComputed().length
+            `/shipment/${this.route.snapshot.params?.id}/verify-products/notifications`
     );
     protected verifiedItemsPercentage = computed(() => {
         if (
@@ -80,8 +71,8 @@ export class VerifyProductsComponent implements OnInit {
             this.packedItemsComputed()?.length
         );
     });
-    protected shipmentIdComputed = computed(() =>
-        Number(this.route.snapshot.params?.id)
+    protected toBeRemovedItemsComputed = computed(
+        () => this.notificationDetailsSignal()?.toBeRemovedItems ?? []
     );
 
     @ViewChild('scanUnitNumberProductCode')
@@ -97,31 +88,32 @@ export class VerifyProductsComponent implements OnInit {
         private matDialog: MatDialog,
         private toaster: ToastrImplService
     ) {
-        this.store
-            .select(getAuthState)
-            .pipe(take(1))
-            .subscribe((auth) => this.loggedUserIdSignal.set(auth.id));
+        super(
+            route,
+            router,
+            store,
+            shipmentService,
+            toaster,
+            productIconService
+        );
     }
 
     ngOnInit(): void {
-        this.triggerFetchData();
+        this.subscribeTriggerFetchData();
     }
 
-    triggerFetchData(): void {
-        forkJoin({
-            shipment: this.shipmentService.getShipmentById(
-                this.shipmentIdComputed()
-            ),
-            verification:
-                this.shipmentService.getShipmentVerificationDetailsById(
-                    this.shipmentIdComputed()
-                ),
-        }).subscribe(({ shipment, verification }) => {
-            this.shipmentSignal.set(shipment.data?.getShipmentDetailsById);
-            this.verificationSignal.set(
-                verification.data?.getShipmentVerificationDetailsById
-            );
-        });
+    subscribeTriggerFetchData(): void {
+        super
+            .triggerFetchData()
+            .subscribe(({ shipment, verification, notificationDetails }) => {
+                this.shipmentSignal.set(shipment.data?.getShipmentDetailsById);
+                this.verificationSignal.set(
+                    verification.data?.getShipmentVerificationDetailsById
+                );
+                this.notificationDetailsSignal.set(
+                    notificationDetails.data.getNotificationDetailsByShipmentId
+                );
+            });
     }
 
     verifyItem(item: VerifyFilledProductDto): void {
