a9d7ce90f3a4177a4ae85ecfc94798f32927ca02
felipe.campos@arc-one.com <felipe.campos@arc-one.com>
2024-11-28 18:43:24 -0300

    feat(R20-310): ui enhancements - custom 2 digit (day, month) date adapter

--- ui/src/app/shared/components/date-range-picker/date-range-picker.component.ts
+++ ui/src/app/shared/components/date-range-picker/date-range-picker.component.ts
@@ -2,13 +2,28 @@ import { CommonModule } from '@angular/common';
 import { AfterContentInit, Component, Input, OnDestroy } from '@angular/core';
 import { FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
-import { provideNativeDateAdapter } from '@angular/material/core';
+import {
+    DateAdapter,
+    MAT_DATE_FORMATS,
+    provideNativeDateAdapter,
+} from '@angular/material/core';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { Subject, distinctUntilChanged, takeUntil } from 'rxjs';
+import { TwoDigitsDateAdapter } from '../../adapter/two-digits-date.adapter';
+
+export const CUSTOM_DATE_FORMATS = {
+    parse: { dateInput: 'MM/DD/YYYY' },
+    display: {
+        dateInput: 'MM/DD/YYYY',
+        monthYearLabel: 'MMM YYYY',
+        dateA11yLabel: 'MM/DD/YYYY',
+        monthYearA11yLabel: 'MMMM YYYY',
+    },
+};
 
 @Component({
     selector: 'app-date-range-picker',
@@ -26,6 +41,10 @@ import { Subject, distinctUntilChanged, takeUntil } from 'rxjs';
         MatDatepickerModule,
     ],
     templateUrl: './date-range-picker.component.html',
+    viewProviders: [
+        { provide: DateAdapter, useClass: TwoDigitsDateAdapter },
+        { provide: MAT_DATE_FORMATS, useValue: CUSTOM_DATE_FORMATS },
+    ],
 })
 export class DateRangePickerComponent implements AfterContentInit, OnDestroy {
     @Input() title!: string;
