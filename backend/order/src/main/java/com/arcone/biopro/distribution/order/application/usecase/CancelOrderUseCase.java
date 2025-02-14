31bc248ba437fd30a9b156c000a9c18b8d968ab4
allanbraga <Allan.MorelliBraga@arc-one.com>
2025-02-14 18:36:13 -0300

    feat(DIS-315): process Cancel orders received through third party application - Back-end Implementation - Event Catalog Documentation

--- backend/order/src/main/java/com/arcone/biopro/distribution/order/application/usecase/CancelOrderUseCase.java
+++ backend/order/src/main/java/com/arcone/biopro/distribution/order/application/usecase/CancelOrderUseCase.java
@@ -4,6 +4,7 @@ import com.arcone.biopro.distribution.order.application.dto.CancelOrderReceivedD
 import com.arcone.biopro.distribution.order.application.exception.DomainNotFoundForKeyException;
 import com.arcone.biopro.distribution.order.domain.event.OrderCancelledEvent;
 import com.arcone.biopro.distribution.order.domain.event.OrderRejectedEvent;
+import com.arcone.biopro.distribution.order.domain.exception.DomainException;
 import com.arcone.biopro.distribution.order.domain.model.CancelOrderCommand;
 import com.arcone.biopro.distribution.order.domain.model.Order;
 import com.arcone.biopro.distribution.order.domain.repository.OrderRepository;
@@ -37,14 +38,20 @@ public class CancelOrderUseCase implements CancelOrderService {
             .then()
             .onErrorResume(error -> {
                 log.error("Not able to process order cancel event {}",error.getMessage());
-                publishOrderRejectedEvent(cancelOrderReceivedDTO.payload().externalId(), error.getMessage());
+                publishOrderRejectedEvent(cancelOrderReceivedDTO.payload().externalId(), error);
                     return Mono.empty();
                 }
             );
     }
 
-    private void publishOrderRejectedEvent(String externalId, String errorMessage) {
-        log.debug("Publishing OrderRejected {} , ID {}", externalId, errorMessage);
+    private void publishOrderRejectedEvent(String externalId, Throwable error) {
+        log.debug("Publishing OrderRejected {} , ID {}", externalId, error.getMessage());
+        var errorMessage = "";
+        if(error instanceof DomainException){
+            errorMessage = ((DomainException) error).getUseCaseMessageType().getMessage();
+        }else{
+            errorMessage = error.getMessage();
+        }
         applicationEventPublisher.publishEvent(new OrderRejectedEvent(externalId, errorMessage));
     }
 
