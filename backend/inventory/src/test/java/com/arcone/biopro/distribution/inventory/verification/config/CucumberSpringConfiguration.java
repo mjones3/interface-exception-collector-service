764a3d1c9749c81c75b31d66570f140d552ceea5
felipe.campos@arc-one.com <felipe.campos@arc-one.com>
2025-02-19 16:19:02 -0300

    feat(R20-333): inventory outbound - Rolling back cucumberspringconfiguration embeddedkafka config

--- backend/inventory/src/test/java/com/arcone/biopro/distribution/inventory/verification/config/CucumberSpringConfiguration.java
+++ backend/inventory/src/test/java/com/arcone/biopro/distribution/inventory/verification/config/CucumberSpringConfiguration.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.kafka.test.context.EmbeddedKafka;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 
