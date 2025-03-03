7d39d587c6c4a29a3a53aa48cd257ecd0025123b
felipe.campos@arc-one.com <felipe.campos@arc-one.com>
2025-03-03 15:18:39 -0300

    feat(R20-591): inventory outbound changes - Added eventId property

--- backend/inventory/src/main/java/com/arcone/biopro/distribution/inventory/adapter/in/listener/EventMessage.java
+++ backend/inventory/src/main/java/com/arcone/biopro/distribution/inventory/adapter/in/listener/EventMessage.java
@@ -2,9 +2,10 @@ package com.arcone.biopro.distribution.inventory.adapter.in.listener;
 
 
 import java.time.ZonedDateTime;
+import java.util.UUID;
 
-public record EventMessage<T>(String eventType, String eventVersion, ZonedDateTime occurredOn, T payload) {
+public record EventMessage<T>(String eventId, String eventType, String eventVersion, ZonedDateTime occurredOn, T payload) {
     public EventMessage(String eventType, String eventVersion, T payload) {
-        this(eventType, eventVersion, ZonedDateTime.now(), payload);
+        this(UUID.randomUUID().toString(), eventType, eventVersion, ZonedDateTime.now(), payload);
     }
 }
