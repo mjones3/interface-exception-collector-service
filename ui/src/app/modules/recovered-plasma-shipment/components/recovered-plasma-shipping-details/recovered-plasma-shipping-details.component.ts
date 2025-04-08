d0106f1ed85eb694fa336e4758672b030506cd3f
Michel Risucci <michel.risucci@arc-one.com>
2025-04-08 14:06:44 -0300

    feat(DIS-338): fixes after rebase with DIS-335

--- ui/src/app/modules/recovered-plasma-shipment/components/recovered-plasma-shipping-details/recovered-plasma-shipping-details.component.ts
+++ ui/src/app/modules/recovered-plasma-shipment/components/recovered-plasma-shipping-details/recovered-plasma-shipping-details.component.ts
@@ -1,18 +1,24 @@
 import { AsyncPipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { ApolloError } from '@apollo/client';
 import { Store } from '@ngrx/store';
 import { ToastrService } from 'ngx-toastr';
 import {
     ProcessHeaderComponent,
     ProcessHeaderService, ToastrImplService,
 } from '@shared';
+import { ProcessHeaderComponent, ProcessHeaderService } from '@shared';
 import { getAuthState } from 'app/core/state/auth/auth.selectors';
 import { ActionButtonComponent } from 'app/shared/components/buttons/action-button.component';
 import { BasicButtonComponent } from 'app/shared/components/buttons/basic-button.component';
 import { ProductIconsService } from 'app/shared/services/product-icon.service';
 import { CookieService } from 'ngx-cookie-service';
 import { take } from 'rxjs';
+import { ToastrService } from 'ngx-toastr';
+import { catchError, map, take, tap } from 'rxjs';
+import { consumeUseCaseNotifications } from '../../../../shared/utils/notification.handling';
+import { FindShipmentRequestDTO } from '../../graphql/query-definitions/shipmentDetails.graphql';
 import { RecoveredPlasmaShipmentResponseDTO } from '../../models/recovered-plasma.dto';
 import { RecoveredPlasmaShipmentCommon } from '../../recovered-plasma-shipment.common';
 import { RecoveredPlasmaService } from '../../services/recovered-plasma.service';
@@ -109,5 +115,4 @@ export class RecoveredPlasmaShippingDetailsComponent
                 );
             });
     }
-
 }
