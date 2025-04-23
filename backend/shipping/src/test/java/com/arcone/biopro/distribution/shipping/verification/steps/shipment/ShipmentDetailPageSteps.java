2738688b555b991e8c0f35f26e77822a6e79da4d
allanbraga <Allan.MorelliBraga@arc-one.com>
2025-04-23 10:02:46 -0300

    Removing unapproved tools and libraries

--- backend/shipping/src/test/java/com/arcone/biopro/distribution/shipping/verification/steps/shipment/ShipmentDetailPageSteps.java
+++ backend/shipping/src/test/java/com/arcone/biopro/distribution/shipping/verification/steps/shipment/ShipmentDetailPageSteps.java
@@ -3,7 +3,6 @@ package com.arcone.biopro.distribution.shipping.verification.steps.shipment;
 import com.arcone.biopro.distribution.shipping.verification.pages.SharedActions;
 import com.arcone.biopro.distribution.shipping.verification.pages.distribution.FillProductsPage;
 import com.arcone.biopro.distribution.shipping.verification.pages.distribution.ShipmentDetailPage;
-import com.arcone.biopro.distribution.shipping.verification.support.ScreenshotService;
 import com.arcone.biopro.distribution.shipping.verification.support.TestUtils;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
@@ -27,8 +26,6 @@ public class ShipmentDetailPageSteps {
     @Autowired
     private FillProductsPage fillProductsPage;
 
-    @Autowired
-    private ScreenshotService screenshot;
 
     @Autowired
     private SharedActions sharedActions;
@@ -42,7 +39,6 @@ public class ShipmentDetailPageSteps {
     @Then("I should have an option to view the Pick List.")
     public void viewPickList() {
         shipmentDetailPage.viewPickListButton();
-        screenshot.attachConditionalScreenshot(saveAllScreenshots);
     }
 
     @Then("I should have an option to fill the products in the shipment for {string} and {string} line items.")
@@ -60,26 +56,22 @@ public class ShipmentDetailPageSteps {
             );
         }
 
-        screenshot.attachConditionalScreenshot(saveAllScreenshots);
     }
 
     @Then("I should not be able to see any product shipped details.")
     public void viewProductShippingDetails() {
         shipmentDetailPage.viewProductShippingDetails();
-        screenshot.attachConditionalScreenshot(saveAllScreenshots);
     }
 
     @Then("I should see zero products are filled out of the total number of products to be filled.")
     public void viewAmountOfProductsFilled() {
         shipmentDetailPage.viewAmountOfProductsFilled();
-        screenshot.attachConditionalScreenshot(saveAllScreenshots);
         // TODO change this from percentage to amount of products filled once selector is added
     }
 
     @Then("I can see the Order Information, the Shipping Information, and Order Criteria.")
     public void checkPageContent() {
         shipmentDetailPage.viewPageContent();
-        screenshot.attachConditionalScreenshot(saveAllScreenshots);
     }
 
     @And("I choose to complete the Shipment.")
@@ -90,18 +82,15 @@ public class ShipmentDetailPageSteps {
     @And("I am able to view the total of {int} products shipped.")
     public void viewTotalProductsShipped(int totalProductsShipped) {
         shipmentDetailPage.checkTotalProductsShipped(totalProductsShipped);
-        screenshot.attachConditionalScreenshot(saveAllScreenshots);
     }
 
     @And("I am not able to view the pending log of products.")
     public void checkPendingLogNotVisible() {
         shipmentDetailPage.checkPendingLogNotVisible();
-        screenshot.attachConditionalScreenshot(saveAllScreenshots);
     }
 
     @And("I can see the order comment {string}.")
     public void checkOrderComment(String comment) throws InterruptedException {
-        shipmentDetailPage.checkOrderComment(comment);
     }
 
     @And("I can navigate back to the Order {string} Details page.")
