5f8a5c507d67c7c27ae22c682a863aa96ab1c6be
allanbraga <Allan.MorelliBraga@arc-one.com>
2025-05-02 16:04:58 -0300

    feat(DIS-343): Print Carton Packing Slip - Back-end Implementation

--- backend/recoveredplasmashipping/src/main/db/sql/rc/V2025.04.30.0002__load_rps_packing_slip_data.sql
+++ backend/recoveredplasmashipping/src/main/db/sql/rc/V2025.04.30.0002__load_rps_packing_slip_data.sql
@@ -8,5 +8,6 @@ VALUES ('RPS_CARTON_PACKING_SLIP','BLOOD_CENTER_NAME','American Red Cross'),
        ('RPS_CARTON_PACKING_SLIP','USE_LICENSE_NUMBER','Y'),
        ('RPS_CARTON_PACKING_SLIP','DATE_TIME_FORMAT','MM/dd/yyyy HH:mm'),
        ('RPS_CARTON_PACKING_SLIP','DATE_FORMAT','MM/dd/yyyy'),
-       ('RPS_CARTON_PACKING_SLIP','TESTING_STATEMENT_TXT','Products packed, inspected and found satisfactory by: %s');
+       ('RPS_CARTON_PACKING_SLIP','ADDRESS_FORMAT','{address} {city}, {state}, {zipCode} {county}'),
+       ('RPS_CARTON_PACKING_SLIP','TESTING_STATEMENT_TXT','Products packed, inspected and found satisfactory by: {employeeName}');
 
