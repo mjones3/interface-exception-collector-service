7f1b8ad479dd044d9a84e642cdeed693cbf0e1df
Jesus Maldonado <jesus.donovan@arc-one.com>
2025-05-14 16:09:59 -0600

    feat(LAB-456): inventory complete app springwolf annotation

--- backend/inventory/src/main/java/com/arcone/biopro/distribution/inventory/adapter/in/listener/completed/ProductCompletedMessage.java
+++ backend/inventory/src/main/java/com/arcone/biopro/distribution/inventory/adapter/in/listener/completed/ProductCompletedMessage.java
@@ -2,9 +2,34 @@ package com.arcone.biopro.distribution.inventory.adapter.in.listener.completed;
 
 import com.arcone.biopro.distribution.inventory.adapter.in.listener.common.Volume;
 import com.arcone.biopro.distribution.inventory.domain.model.enumeration.AboRhType;
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import io.swagger.v3.oas.annotations.media.Schema;
 
-public record ProductCompletedMessage(String unitNumber,
-                                      String productCode,
-                                      Volume volume,
-                                      Volume anticoagulantVolume,
-                                      AboRhType aboRh) {}
+@JsonTypeInfo(
+    use = JsonTypeInfo.Id.NAME,
+    property = "type"
+)
+@JsonSubTypes({
+    @JsonSubTypes.Type(value = ProductCompletedMessage.class, name = "ApheresisPlasmaProductCompleted"),
+    @JsonSubTypes.Type(value = ProductCompletedMessage.class, name = "ApheresisRBCProductCompleted"),
+    @JsonSubTypes.Type(value = ProductCompletedMessage.class, name = "ApheresisPlateletProductCompleted"),
+    @JsonSubTypes.Type(value = ProductCompletedMessage.class, name = "WholeBloodProductCompleted")
+})
+public record ProductCompletedMessage(
+    @Schema(description = "Unit number identifier")
+    String unitNumber,
+
+    @Schema(description = "Product code")
+    String productCode,
+
+    @Schema(description = "Volume of the product")
+    Volume volume,
+
+    @Schema(description = "Anticoagulant volume of the product")
+    Volume anticoagulantVolume
+    ,
+    @Schema(description = "Abo RH Type of the product")
+    AboRhType aboRh
+) {
+}
--- backend/inventory/src/main/java/com/arcone/biopro/distribution/inventory/adapter/in/listener/recovered/ProductRecoveredMessage.java
+++ backend/inventory/src/main/java/com/arcone/biopro/distribution/inventory/adapter/in/listener/recovered/ProductRecoveredMessage.java
@@ -1,8 +1,25 @@
 package com.arcone.biopro.distribution.inventory.adapter.in.listener.recovered;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 import java.time.ZonedDateTime;
 
-public record ProductRecoveredMessage(String unitNumber,
-                                      String productCode,
-                                      String performedBy,
-                                      ZonedDateTime createDate) {}
+@Schema(
+    name = "ProductRecovered",
+    title = "ProductRecovered",
+    description = "Message for recovered product process"
+)
+public record ProductRecoveredMessage(
+    @Schema(description = "Unit number identifier")
+    String unitNumber,
+
+    @Schema(description = "Product code")
+    String productCode,
+
+    @Schema(description = "User who performed the recovery")
+    String performedBy,
+
+    @Schema(description = "Date and time when recovery was created")
+    ZonedDateTime createDate
+) {
+}
