fbed7cbeff5dd7bd81796851f86e8a5901b67344
Jesus Maldonado <jesus.donovan@arc-one.com>
2025-05-15 14:46:06 -0600

    feat(LAB-456): inventory productcompletedevent springwolf annotation

--- backend/inventory/src/main/java/com/arcone/biopro/distribution/inventory/adapter/in/listener/completed/ProductCompletedMessage.java
+++ backend/inventory/src/main/java/com/arcone/biopro/distribution/inventory/adapter/in/listener/completed/ProductCompletedMessage.java
@@ -2,20 +2,13 @@ package com.arcone.biopro.distribution.inventory.adapter.in.listener.completed;
 
 import com.arcone.biopro.distribution.inventory.adapter.in.listener.common.Volume;
 import com.arcone.biopro.distribution.inventory.domain.model.enumeration.AboRhType;
-import com.fasterxml.jackson.annotation.JsonSubTypes;
-import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import io.swagger.v3.oas.annotations.media.Schema;
 
-@JsonTypeInfo(
-    use = JsonTypeInfo.Id.NAME,
-    property = "type"
+@Schema(
+    name = "ApheresisPlasmaProductCompleted",
+    title = "ApheresisPlasmaProductCompleted",
+    description = "Message for completed ApheresisPlasmaProductCompleted process"
 )
-@JsonSubTypes({
-    @JsonSubTypes.Type(value = ProductCompletedMessage.class, name = "ApheresisPlasmaProductCompleted"),
-    @JsonSubTypes.Type(value = ProductCompletedMessage.class, name = "ApheresisRBCProductCompleted"),
-    @JsonSubTypes.Type(value = ProductCompletedMessage.class, name = "ApheresisPlateletProductCompleted"),
-    @JsonSubTypes.Type(value = ProductCompletedMessage.class, name = "WholeBloodProductCompleted")
-})
 public record ProductCompletedMessage(
     @Schema(description = "Unit number identifier")
     String unitNumber,
