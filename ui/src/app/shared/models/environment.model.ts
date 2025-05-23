bbe8eaaa1ea9daea1353a6ab8702ff86365b5b7d
Michel Risucci <michel.risucci@arc-one.com>
2025-05-23 11:26:04 -0300

    feat(DIS-404): added print label service

--- ui/src/app/shared/models/environment.model.ts
+++ ui/src/app/shared/models/environment.model.ts
@@ -2,6 +2,7 @@ import { ProcessProductVersionModel } from './process-product-version.model';
 
 export interface Environment {
     serverApiURL: string;
+    agentApiURL: string;
     properties: Map<string, string>;
     productVersion: ProcessProductVersionModel;
     realm: string;
