95171cf2ff87e00e0d8e1768d71c8b4aa1d7cfd6
Rene Rubio <rene.rubio@arc-one.com>
2025-05-23 11:55:25 -0400

    feat: adding embeddedKafka to cucumber configuration

--- backend/inventory/src/test/java/com/arcone/biopro/distribution/inventory/verification/config/CucumberSpringConfiguration.java
+++ backend/inventory/src/test/java/com/arcone/biopro/distribution/inventory/verification/config/CucumberSpringConfiguration.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.kafka.test.context.EmbeddedKafka;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 
@@ -15,6 +16,7 @@ import org.springframework.test.context.TestPropertySource;
 @SpringBootTest(classes = BioProApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @TestPropertySource(locations="classpath:application-test.properties")
 @ActiveProfiles("test")
+@EmbeddedKafka(partitions = 1, brokerProperties = {"listeners=PLAINTEXT://localhost:9092", "port=9092"})
 public class CucumberSpringConfiguration {
 
     @Autowired
