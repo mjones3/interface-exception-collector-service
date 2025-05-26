04480ceeaf18ee3b8813e30f7014182e98c8c803
Benjamin Pinto <benjamin.pinto@arc-one.com>
2025-05-26 10:37:35 -0400

    feat(DIS-402): keycloak upgrade - Fix cucumber suite file; - Remove unwanted config file;

--- backend/shipping/src/test/java/com/arcone/biopro/distribution/shipping/verification/CucumberSuite.java
+++ backend/shipping/src/test/java/com/arcone/biopro/distribution/shipping/verification/CucumberSuite.java
@@ -9,7 +9,6 @@ import org.springframework.test.context.ActiveProfiles;
 
 import static io.cucumber.core.options.Constants.FILTER_TAGS_PROPERTY_NAME;
 import static io.cucumber.core.options.Constants.GLUE_PROPERTY_NAME;
-import static io.cucumber.core.options.Constants.PLUGIN_PROPERTY_NAME;
 
 @Suite
 @IncludeEngines("cucumber")
@@ -17,16 +16,12 @@ import static io.cucumber.core.options.Constants.PLUGIN_PROPERTY_NAME;
 @SelectDirectories("src/test/java/com/arcone/biopro/distribution/shipping/verification/features")
 @ConfigurationParameters({
     @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.arcone.biopro.distribution.shipping.verification"),
-    @ConfigurationParameter(key = FILTER_TAGS_PROPERTY_NAME, value = "(@api or @ui) and not @disabled"),
-    @ConfigurationParameter(
-        key = PLUGIN_PROPERTY_NAME,
-        value = "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"
-    ),
+    @ConfigurationParameter(key = FILTER_TAGS_PROPERTY_NAME, value = "@api and not @disabled"),
     @ConfigurationParameter(key = "cucumber.execution.parallel.enabled", value = "true"),
     @ConfigurationParameter(key = "cucumber.execution.execution-mode.feature", value = "same_thread"),
     @ConfigurationParameter(key = "cucumber.execution.parallel.config.strategy", value = "fixed"),
-    @ConfigurationParameter(key = "cucumber.execution.parallel.config.fixed.parallelism", value = "5"),
-    @ConfigurationParameter(key = "cucumber.execution.parallel.config.fixed.max-pool-size", value = "15")
+    @ConfigurationParameter(key = "cucumber.execution.parallel.config.fixed.parallelism", value = "1"),
+    @ConfigurationParameter(key = "cucumber.execution.parallel.config.fixed.max-pool-size", value = "1")
 })
 public class CucumberSuite {
 }
