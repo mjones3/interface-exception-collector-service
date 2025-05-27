f47c2e8c1f9cfc853d787e4fe531c82dc736bdab
allanbraga <Allan.MorelliBraga@arc-one.com>
2025-05-27 09:51:53 -0300

    feat(DIS-358): Automation refactor to run @api suite in pipeline - Add application properties by env

--- backend/receiving/src/test/java/com/arcone/biopro/distribution/receiving/verification/CucumberSuite.java
+++ backend/receiving/src/test/java/com/arcone/biopro/distribution/receiving/verification/CucumberSuite.java
@@ -5,7 +5,6 @@ import org.junit.platform.suite.api.ConfigurationParameters;
 import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectDirectories;
 import org.junit.platform.suite.api.Suite;
-import org.springframework.test.context.ActiveProfiles;
 
 import static io.cucumber.core.options.Constants.FILTER_TAGS_PROPERTY_NAME;
 import static io.cucumber.core.options.Constants.GLUE_PROPERTY_NAME;
@@ -20,7 +19,6 @@ import static io.cucumber.junit.platform.engine.Constants.JUNIT_PLATFORM_NAMING_
     @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "json:target/cucumber/cucumber.json"),
     @ConfigurationParameter(key=PLUGIN_PROPERTY_NAME, value = "rerun:target/cucumber/rerun.txt"),
     @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.arcone.biopro.distribution.receiving.verification"),
-    @ConfigurationParameter(key = FILTER_TAGS_PROPERTY_NAME, value = "not @disabled"),
     @ConfigurationParameter(key = PLUGIN_PUBLISH_QUIET_PROPERTY_NAME, value = "true"),
     @ConfigurationParameter(key = JUNIT_PLATFORM_NAMING_STRATEGY_PROPERTY_NAME, value = "long"),
     @ConfigurationParameter(key = "cucumber.execution.parallel.enabled", value = "true"),
@@ -29,6 +27,5 @@ import static io.cucumber.junit.platform.engine.Constants.JUNIT_PLATFORM_NAMING_
     @ConfigurationParameter(key = "cucumber.execution.parallel.config.fixed.parallelism", value = "1"),
     @ConfigurationParameter(key = "cucumber.execution.parallel.config.fixed.max-pool-size", value = "1")
 })
-@ActiveProfiles("AUTOMATION")
 public class CucumberSuite {
 }
