4310c106a4689e316283ae8d32c89784d13a134e
Anderson Paulino <anderson.paulino@arc-one.com>
2025-05-29 16:51:01 -0400

    feat(LAB-503: implement the listener

--- backend/inventory/src/main/db/sql/common/V2025.05.29.0002__create_property_table.sql
+++ backend/inventory/src/main/db/sql/common/V2025.05.29.0002__create_property_table.sql
@@ -1,6 +1,10 @@
 CREATE TABLE inventory.bld_inventory_property
 (
+    id                          uuid NOT NULL,
     key                         VARCHAR(255) NOT NULL,
     value                       VARCHAR(255) NOT NULL,
-    CONSTRAINT pk_bld_inventory_property PRIMARY KEY (key)
+    inventory_id                          uuid NOT NULL,
+    create_date                 TIMESTAMP WITH TIME ZONE NOT NULL,
+    modification_date           TIMESTAMP WITH TIME ZONE NOT NULL,
+    CONSTRAINT pk_bld_inventory_property PRIMARY KEY (id)
 );
