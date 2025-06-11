0f86e86b3bacc65e27842f5bcbe7930b7fedc032
allanbraga <Allan.MorelliBraga@arc-one.com>
2025-06-11 18:43:42 -0300

    feat(DIS-412): Imports - Enter Imported Products Information - Add products Back-end implementation

--- backend/receiving/src/main/java/com/arcone/biopro/distribution/receiving/application/dto/UseCaseMessageType.java
+++ backend/receiving/src/main/java/com/arcone/biopro/distribution/receiving/application/dto/UseCaseMessageType.java
@@ -16,7 +16,7 @@ public enum UseCaseMessageType {
     VALIDATE_TRANSIT_TIME_SYSTEM_ERROR(4,  UseCaseNotificationType.SYSTEM, "Not able to validate transit time. Contact Support."),
     IMPORT_CREATE_SUCCESS(5,  UseCaseNotificationType.SYSTEM, "Import created successfully."),
     VALIDATE_BARCODE_SYSTEM_ERROR(6,  UseCaseNotificationType.SYSTEM, "Not able to validate barcode. Contact Support."),
-    IMPORT_ITEM_CREATE_SUCCESS(7,  UseCaseNotificationType.SYSTEM, "Product added successfully.");
+    IMPORT_ITEM_CREATE_SUCCESS(7,  UseCaseNotificationType.SUCCESS, "Product added successfully.");
 
     Integer code;
     UseCaseNotificationType type;
