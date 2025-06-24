5bcfdda4d17dc66ded0dafc5a7b5998cbea8e1ab
Felipe Campos <felipe.campos@arc-one.com>
2025-06-24 12:59:10 -0300

    feat(R20-633): Order TransactionId - some refactoring

--- backend/eventbridge/src/main/java/com/arcone/biopro/distribution/eventbridge/domain/service/OrderService.java
+++ backend/eventbridge/src/main/java/com/arcone/biopro/distribution/eventbridge/domain/service/OrderService.java
@@ -2,11 +2,12 @@ package com.arcone.biopro.distribution.eventbridge.domain.service;
 
 import com.arcone.biopro.distribution.eventbridge.application.dto.OrderCancelledPayload;
 import com.arcone.biopro.distribution.eventbridge.application.dto.OrderModifiedPayload;
-import com.arcone.biopro.distribution.eventbridge.application.dto.OrderPayload;
+import com.arcone.biopro.distribution.eventbridge.application.dto.OrderRejectedPayload;
 import reactor.core.publisher.Mono;
 
 public interface OrderService {
 
     Mono<Void> processOrderCancelledEvent(OrderCancelledPayload orderPayload);
     Mono<Void> processOrderModifiedEvent(OrderModifiedPayload orderPayload);
+    Mono<Void> processOrderRejectedEvent(OrderRejectedPayload orderPayload);
 }
