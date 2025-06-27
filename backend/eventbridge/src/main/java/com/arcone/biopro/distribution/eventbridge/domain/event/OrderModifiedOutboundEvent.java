867549b6b3851c3569aec80fcbe27ee5a4bd949e
Felipe Campos <felipe.campos@arc-one.com>
2025-06-27 14:51:19 -0300

    feat(R20-633): Order TransactionId - Order outbound event domain class refactoring

--- backend/eventbridge/src/main/java/com/arcone/biopro/distribution/eventbridge/domain/event/OrderModifiedOutboundEvent.java
+++ backend/eventbridge/src/main/java/com/arcone/biopro/distribution/eventbridge/domain/event/OrderModifiedOutboundEvent.java
@@ -2,45 +2,16 @@ package com.arcone.biopro.distribution.eventbridge.domain.event;
 
 import com.arcone.biopro.distribution.eventbridge.domain.model.OrderModifiedOutbound;
 
-import java.time.Instant;
-import java.util.UUID;
+public class OrderModifiedOutboundEvent extends OrderOutboundEvent<OrderModifiedOutbound> {
 
-public class OrderModifiedOutboundEvent implements DomainEvent {
-
-    private final UUID eventId;
-    private final Instant occurredOn;
-    private final static String eventVersion = "1.0";
     private final static String eventType = "OrderModifiedOutbound";
-    private OrderModifiedOutbound payload;
 
     public OrderModifiedOutboundEvent(OrderModifiedOutbound payload) {
-        this.eventId = UUID.randomUUID();
-        this.occurredOn = Instant.now();
-        this.payload = payload;
-    }
-
-    @Override
-    public UUID getEventId() {
-        return this.eventId;
-    }
-
-    @Override
-    public Instant getOccurredOn() {
-        return this.occurredOn;
+        super(payload);
     }
 
     @Override
     public String getEventType() {
         return this.eventType;
-    }
-
-    @Override
-    public String getEventVersion() {
-        return this.eventVersion;
-    }
-
-    @Override
-    public OrderModifiedOutbound getPayload() {
-        return this.payload;
     }
 }
