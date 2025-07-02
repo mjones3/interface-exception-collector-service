048055afd4bcea71d83d664fa4b7b585aa5268e9
Benjamin Pinto <benjamin.pinto@arc-one.com>
2025-07-02 09:14:14 -0400

    feat(DIS-495): order Service - Location Local data store - add order service lk_location and lk_location_properties tables; - add repository, mapper, and entity for the new tables;

---
+++ backend/order/src/main/java/com/arcone/biopro/distribution/order/infrastructure/persistence/LocationRepositoryImpl.java
@@ -0,0 +1,42 @@
+package com.arcone.biopro.distribution.order.infrastructure.persistence;
+
+import com.arcone.biopro.distribution.order.domain.model.Location;
+import com.arcone.biopro.distribution.order.domain.repository.LocationRepository;
+import com.arcone.biopro.distribution.order.infrastructure.mapper.LocationEntityMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Repository
+@RequiredArgsConstructor
+public class LocationRepositoryImpl implements LocationRepository {
+
+    private final LocationEntityRepository locationEntityRepository;
+    private final LocationPropertyEntityRepository locationPropertyEntityRepository;
+    private final LocationEntityMapper locationEntityMapper;
+
+
+
+    @Override
+    public Flux<Location> findAll() {
+        return locationEntityRepository.findAllByActiveIsTrueOrderByNameAsc()
+            .map(locationEntity -> locationEntityMapper.toDomain(locationEntity,null));
+    }
+
+    @Override
+    public Mono<Location> findOneById(Long id) {
+        return locationEntityRepository.findById(id)
+            .flatMap(locationEntity -> locationPropertyEntityRepository.findByLocationId(locationEntity.getId())
+                .collectList()
+                .map(locationProperties -> locationEntityMapper.toDomain(locationEntity,locationProperties)));
+    }
+
+    @Override
+    public Mono<Location> findOneByCode(String code) {
+        return locationEntityRepository.findByCode(code)
+            .flatMap(locationEntity -> locationPropertyEntityRepository.findByLocationId(locationEntity.getId())
+                .collectList()
+                .map(locationProperties -> locationEntityMapper.toDomain(locationEntity,locationProperties)));
+    }
+}
