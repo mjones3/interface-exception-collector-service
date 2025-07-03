5521a4fc6cf468a2e7fc1c4be295ef945cdec1f8
Rene Rubio <rene.rubio@arc-one.com>
2025-07-03 09:40:47 -0400

    feat: fixing test config

--- backend/irradiation/src/test/java/com/arcone/biopro/distribution/irradiation/verification/config/CucumberSuite.java
+++ backend/irradiation/src/test/java/com/arcone/biopro/distribution/irradiation/verification/config/CucumberSuite.java
@@ -9,7 +9,7 @@ import static io.cucumber.junit.platform.engine.Constants.JUNIT_PLATFORM_NAMING_
 @IncludeEngines("cucumber")
 @SelectDirectories({"src/test/java/com/arcone/biopro/distribution/irradiation/verification/ui/features","src/test/java/com/arcone/biopro/distribution/irradiation/verification/api/features"})
 @ConfigurationParameters({
-    @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.arcone.biopro.operations.device.verification"),
+    @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.arcone.biopro.distribution.irradiation.verification"),
     @ConfigurationParameter(key = FILTER_TAGS_PROPERTY_NAME, value = "not (@Disabled)"),
     @ConfigurationParameter(key = PLUGIN_PUBLISH_QUIET_PROPERTY_NAME, value = "true"),
     @ConfigurationParameter(key = JUNIT_PLATFORM_NAMING_STRATEGY_PROPERTY_NAME, value = "long")
