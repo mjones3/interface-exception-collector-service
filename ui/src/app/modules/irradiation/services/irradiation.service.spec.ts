ddc0def8f858b6aab2654036953d17280139984e
YovannysCorales <Yovannys.Corales@arc-one.com>
2025-07-06 12:02:49 -0400

    feat(LAB-573): fixing unit test

--- ui/src/app/modules/irradiation/services/irradiation.service.spec.ts
+++ ui/src/app/modules/irradiation/services/irradiation.service.spec.ts
@@ -1,12 +1,21 @@
 import { TestBed } from '@angular/core/testing';
-
 import { IrradiationService } from './irradiation.service';
+import { DynamicGraphqlPathService } from '../../../core/services/dynamic-graphql-path.service';
 
 describe('IrradiationService', () => {
   let service: IrradiationService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    const mockDynamicGraphqlPathService = {
+      executeQuery: jest.fn(),
+      executeMutation: jest.fn()
+    };
+
+    TestBed.configureTestingModule({
+      providers: [
+        { provide: DynamicGraphqlPathService, useValue: mockDynamicGraphqlPathService }
+      ]
+    });
     service = TestBed.inject(IrradiationService);
   });
 
