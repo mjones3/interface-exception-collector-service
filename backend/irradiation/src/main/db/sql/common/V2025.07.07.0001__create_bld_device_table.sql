f65c3ebf892cc9cc9b5f2c4c5a420c2746b634b6
Fabio Sakiyama <fabio.sakiyama@arc-one.com>
2025-07-07 18:31:17 -0300

    feat(lab-574): fixing varchar sizes

--- backend/irradiation/src/main/db/sql/common/V2025.07.07.0001__create_bld_device_table.sql
+++ backend/irradiation/src/main/db/sql/common/V2025.07.07.0001__create_bld_device_table.sql
@@ -1,6 +1,6 @@
 CREATE TABLE irradiation.bld_device (
     id BIGSERIAL PRIMARY KEY,
-    device_id VARCHAR(255) NOT NULL,
-    location VARCHAR(255) NOT NULL,
-    status VARCHAR(255) NOT NULL
+    device_id VARCHAR(100) NOT NULL,
+    location VARCHAR(50) NOT NULL,
+    status VARCHAR(20) NOT NULL
 );
