cc0171e80858079c23a8d20388f8e471449461e7
Vanessa Mara <vanessa.mara@arc-one.com>
2025-07-09 17:13:07 -0300

    feat(LAB-294): adding new product and products

--- backend/inventory/src/main/db/sql/common/V2025.07.09.0001__add_missing_product_families.sql
+++ backend/inventory/src/main/db/sql/common/V2025.07.09.0001__add_missing_product_families.sql
@@ -1,9 +1,3 @@
--- Add missing product families
-INSERT INTO inventory.lk_product_family (product_family, time_frame) VALUES ('WASHED_APHERESIS_PLATELETS', 0);
-INSERT INTO inventory.lk_product_family (product_family, time_frame) VALUES ('WASHED_RED_BLOOD_CELLS', 0);
-INSERT INTO inventory.lk_product_family (product_family, time_frame) VALUES ('WASHED_PRT_APHERESIS_PLATELETS', 0);
-
-
 -- Add temperature categories for washed products
 INSERT INTO inventory.lk_temperature_category_product_code (product_code, temperature_category, active, create_date, modification_date) VALUES ('E354100', 'ROOM_TEMPERATURE', true, now(), now());
 INSERT INTO inventory.lk_temperature_category_product_code (product_code, temperature_category, active, create_date, modification_date) VALUES ('E354300', 'ROOM_TEMPERATURE', true, now(), now());
