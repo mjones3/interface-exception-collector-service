62c9a66e9019311b2d4723055c6573f366adab7e
YovannysCorales <Yovannys.Corales@arc-one.com>
2025-07-09 17:31:42 -0400

    feat(LAB-574): adding read config endpoint

--- backend/irradiation/src/main/java/com/arcone/biopro/distribution/irradiation/adapter/in/web/mapper/ConfigurationDTOMapper.java
+++ backend/irradiation/src/main/java/com/arcone/biopro/distribution/irradiation/adapter/in/web/mapper/ConfigurationDTOMapper.java
@@ -2,8 +2,6 @@ package com.arcone.biopro.distribution.irradiation.adapter.in.web.mapper;
 
 import com.arcone.biopro.distribution.irradiation.adapter.in.web.dto.ConfigurationResponseDTO;
 import com.arcone.biopro.distribution.irradiation.domain.model.Configuration;
-import com.arcone.biopro.manufacturing.labeling.adapter.in.web.dto.ConfigurationResponseDTO;
-import com.arcone.biopro.manufacturing.labeling.domain.model.vo.Configuration;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
