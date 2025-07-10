244585664c96982bd1f33112831e6ae196f23f42
YovannysCorales <Yovannys.Corales@arc-one.com>
2025-07-09 22:41:16 -0400

    feat(LAB-574): refactoring custom exception resolver

--- backend/irradiation/src/main/java/com/arcone/biopro/distribution/irradiation/infrastructure/config/CustomExceptionResolver.java
+++ backend/irradiation/src/main/java/com/arcone/biopro/distribution/irradiation/infrastructure/config/CustomExceptionResolver.java
@@ -1,46 +1,43 @@
 package com.arcone.biopro.distribution.irradiation.infrastructure.config;
 
-import com.arcone.biopro.distribution.irradiation.domain.exception.ConfigurationNotFoundException;
+import com.arcone.biopro.distribution.irradiation.domain.exception.CustomBaseException;
 import graphql.GraphQLError;
 import graphql.GraphqlErrorBuilder;
 import graphql.schema.DataFetchingEnvironment;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.graphql.execution.DataFetcherExceptionResolverAdapter;
-import org.springframework.graphql.execution.ErrorType;
 import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Objects;
 
 @Component
+@Slf4j
 public class CustomExceptionResolver extends DataFetcherExceptionResolverAdapter {
-
-
-    Map<Class<? extends RuntimeException>, ErrorType> errorTypeMap = new HashMap<>() {{
-            put(IllegalArgumentException.class, ErrorType.BAD_REQUEST);
-            put(ConfigurationNotFoundException.class, ErrorType.NOT_FOUND);
-
-        }};
 
     @Override
     protected GraphQLError resolveToSingleError(Throwable ex, DataFetchingEnvironment env) {
-        
-        Throwable rootCause = getRootCause(ex);
-        ErrorType errorType = Objects.requireNonNullElse(errorTypeMap.get(rootCause.getClass()), ErrorType.INTERNAL_ERROR);
+        if (ex instanceof CustomBaseException) {
+            return buildError((CustomBaseException) ex, env);
+        }
+        return buildError(new CustomBaseException(ex), env);
+    }
+
+    private GraphQLError buildError(CustomBaseException ex,
+                                    DataFetchingEnvironment env) {
+
+        log.error("Error occurred at {}: {}", env.getExecutionStepInfo().getPath(), ex.getMessage(), ex.getCause());
+
+        Map<String, Object> extensions = new HashMap<>();
+        extensions.put("type", ex.getType());
+        extensions.put("classification", ex.getClassification());
+
 
         return GraphqlErrorBuilder.newError()
-            .errorType(errorType)
-            .message(rootCause.getMessage())
+            .message(ex.getMessage())
             .path(env.getExecutionStepInfo().getPath())
             .location(env.getField().getSourceLocation())
+            .extensions(extensions)
             .build();
-    }
-    
-    private Throwable getRootCause(Throwable ex) {
-        Throwable cause = ex;
-        while (cause.getCause() != null && cause.getCause() != cause) {
-            cause = cause.getCause();
-        }
-        return cause;
     }
 }
