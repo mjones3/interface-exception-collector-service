ed6588ebd3e56ee17c3f7ba77a58421c540f77df
allanbraga <Allan.MorelliBraga@arc-one.com>
2025-07-10 15:53:09 -0300

    feat(DIS-452): Fill Unlabeled Products for Internal Transfer order - Back-end Implementation

--- backend/shipping/src/main/java/com/arcone/biopro/distribution/shipping/infrastructure/controller/InventoryMockController.java
+++ backend/shipping/src/main/java/com/arcone/biopro/distribution/shipping/infrastructure/controller/InventoryMockController.java
@@ -4,6 +4,7 @@ import com.arcone.biopro.distribution.shipping.application.util.ShipmentServiceM
 import com.arcone.biopro.distribution.shipping.infrastructure.controller.dto.InventoryMockData;
 import com.arcone.biopro.distribution.shipping.infrastructure.controller.dto.InventoryNotificationDTO;
 import com.arcone.biopro.distribution.shipping.infrastructure.controller.dto.InventoryResponseDTO;
+import com.arcone.biopro.distribution.shipping.infrastructure.controller.dto.InventoryValidationByUnitNumberRequest;
 import com.arcone.biopro.distribution.shipping.infrastructure.controller.dto.InventoryValidationRequest;
 import com.arcone.biopro.distribution.shipping.infrastructure.controller.dto.InventoryValidationResponseDTO;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -14,6 +15,7 @@ import org.springframework.core.io.ClassPathResource;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Controller;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.io.IOException;
@@ -30,6 +32,7 @@ import java.util.UUID;
 public class InventoryMockController {
     private final ObjectMapper objectMapper;
     private List<InventoryResponseDTO> inventoryResponseDTOList;
+    private List<InventoryResponseDTO> inventoryByUnitResponseDTOList;
     private static final String DISCARD_COMMENTS_250_CHARS = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec.";
 
     @MessageMapping("validateInventory")
@@ -37,7 +40,7 @@ public class InventoryMockController {
         log.info("Checking inventory {} , {} , {} ", request.unitNumber(), request.productCode(), request.locationCode());
 
         if (inventoryResponseDTOList == null) {
-            initInventoryMockList();
+            inventoryResponseDTOList = initInventoryMockList("mock/inventory/inventory-mock-data.json");
         }
 
             /*| Expired            | W036898786756  | E0701V00    Expired error message     |
@@ -192,37 +195,127 @@ public class InventoryMockController {
                             && inventory.locationCode().equals(request.locationCode());
                     }
                 ).findAny();
-                if (inventoryResponse.isPresent()) {
-
-                    return Mono.just(InventoryValidationResponseDTO
+                return inventoryResponse.map(inventoryResponseDTO -> Mono.just(InventoryValidationResponseDTO
+                    .builder()
+                    .inventoryResponseDTO(inventoryResponseDTO)
+                    .build())).orElseGet(() -> Mono.just(InventoryValidationResponseDTO
+                    .builder()
+                    .inventoryNotificationsDTO(List.of(InventoryNotificationDTO
                         .builder()
-                        .inventoryResponseDTO(inventoryResponse.get())
-                        .build());
-                } else {
-                    return Mono.just(InventoryValidationResponseDTO
-                        .builder()
-                        .inventoryNotificationsDTO(List.of(InventoryNotificationDTO
-                            .builder()
-                            .errorName("INVENTORY_NOT_FOUND_IN_LOCATION")
-                            .errorType("WARN")
-                            .errorCode(1)
-                            .errorMessage(ShipmentServiceMessages.INVENTORY_NOT_FOUND_ERROR)
-                            .build()))
-                        .build());
-                }
+                        .errorName("INVENTORY_NOT_FOUND_IN_LOCATION")
+                        .errorType("WARN")
+                        .errorCode(1)
+                        .errorMessage(ShipmentServiceMessages.INVENTORY_NOT_FOUND_ERROR)
+                        .build()))
+                    .build()));
 
         }
     }
 
-    private void initInventoryMockList() {
+    private List<InventoryResponseDTO> initInventoryMockList(String mockFile) {
         inventoryResponseDTOList = new ArrayList<>();
         try {
-            var fileInputStream = new ClassPathResource("mock/inventory/inventory-mock-data.json").getInputStream();
+            var fileInputStream = new ClassPathResource(mockFile).getInputStream();
             var mockData = objectMapper.readValue(fileInputStream, InventoryMockData.class);
-            inventoryResponseDTOList = mockData.data();
+            return mockData.data();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+
+    @MessageMapping("validateInventoryByUnitNumber")
+    public Flux<InventoryValidationResponseDTO> validateInventoryByUnitNumber(@Payload InventoryValidationByUnitNumberRequest request) {
+        log.debug("Checking inventory by Unit Number {} , {} ", request.unitNumber(), request.locationCode());
+
+        if (inventoryByUnitResponseDTOList == null) {
+            inventoryByUnitResponseDTOList = initInventoryMockList("mock/inventory/inventory-by-unit-mock-data.json");
+        }
+
+        return Flux.fromStream(inventoryByUnitResponseDTOList.stream().filter(inventory -> {
+                return inventory.unitNumber().equals(request.unitNumber())
+                    && inventory.locationCode().equals(request.locationCode());
+            }).map(this::buildResponseDtoByStatus))
+            .switchIfEmpty(Flux.just(InventoryValidationResponseDTO
+                .builder()
+                .inventoryNotificationsDTO(List.of(InventoryNotificationDTO
+                    .builder()
+                    .errorName("INVENTORY_NOT_FOUND_IN_LOCATION")
+                    .errorType("WARN")
+                    .errorCode(1)
+                    .errorMessage(ShipmentServiceMessages.INVENTORY_NOT_FOUND_ERROR)
+                    .build()))
+                .build()));
+    }
+
+
+    private InventoryValidationResponseDTO buildResponseDtoByStatus(InventoryResponseDTO inventoryResponseDTO){
+
+        return switch (inventoryResponseDTO.status()) {
+            case "EXPIRED" -> InventoryValidationResponseDTO
+                .builder()
+                .inventoryResponseDTO(inventoryResponseDTO)
+                .inventoryNotificationsDTO(List.of(InventoryNotificationDTO
+                    .builder()
+                    .errorName("INVENTORY_IS_EXPIRED")
+                    .errorType("INFO")
+                    .errorCode(2)
+                    .errorMessage(ShipmentServiceMessages.INVENTORY_EXPIRED_ERROR)
+                    .action("TRIGGER_DISCARD")
+                    .reason("EXPIRED")
+                    .build())).build();
+            case "DISCARDED" -> InventoryValidationResponseDTO
+                .builder()
+                .inventoryResponseDTO(inventoryResponseDTO)
+                .inventoryNotificationsDTO(List.of(InventoryNotificationDTO
+                    .builder()
+                    .errorName("INVENTORY_IS_DISCARDED")
+                    .errorCode(3)
+                    .errorType("INFO")
+                    .errorMessage(ShipmentServiceMessages.INVENTORY_DISCARDED_ERROR + DISCARD_COMMENTS_250_CHARS)
+                    .build()))
+                .build();
+            case "QUARANTINED" -> InventoryValidationResponseDTO
+                .builder()
+                .inventoryResponseDTO(inventoryResponseDTO)
+                .inventoryNotificationsDTO(List.of(InventoryNotificationDTO
+                    .builder()
+                    .errorType("INFO")
+                    .errorName("INVENTORY_IS_QUARANTINED")
+                    .errorCode(4)
+                    .errorMessage(ShipmentServiceMessages.INVENTORY_QUARANTINED_ERROR)
+                    .details(List.of("ABS Positive", "BCA Unit Needed", "CCP Eligible", "Failed Visual Inspection"
+                        , "Hold Until Expiration", "In Process Hold", "Pending Further Review Inspection",
+                        "Save Plasma for CTS", "Other", "Under Investigation"))
+                    .build()))
+                .build();
+            case "SHIPPED" -> InventoryValidationResponseDTO
+                .builder()
+                .inventoryResponseDTO(inventoryResponseDTO)
+                .inventoryNotificationsDTO(List.of(InventoryNotificationDTO
+                    .builder()
+                    .errorType("INFO")
+                    .errorName("INVENTORY_IS_SHIPPED")
+                    .errorCode(4)
+                    .errorMessage(ShipmentServiceMessages.INVENTORY_NOT_FOUND_ERROR)
+                    .build()))
+                .build();
+            case "LABELED" -> InventoryValidationResponseDTO
+                .builder()
+                .inventoryResponseDTO(inventoryResponseDTO)
+                .inventoryNotificationsDTO(List.of(InventoryNotificationDTO
+                    .builder()
+                    .errorType("WARN")
+                    .errorName("INVENTORY_IS_LABELED")
+                    .errorCode(5)
+                    .errorMessage(ShipmentServiceMessages.INVENTORY_LABELED_ERROR)
+                    .build()))
+                .build();
+            default -> InventoryValidationResponseDTO
+                .builder()
+                .inventoryResponseDTO(inventoryResponseDTO)
+                .build();
+        };
     }
 
 }
