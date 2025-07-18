cdca5cab0926acb00f7381400cb3fa413d353c31
YovannysCorales <Yovannys.Corales@arc-one.com>
2025-07-17 20:34:35 -0400

    feat(LAB-574): fixing conflict

--- backend/irradiation/src/main/java/com/arcone/biopro/distribution/irradiation/adapter/irradiation/IrradiationResource.java
+++ backend/irradiation/src/main/java/com/arcone/biopro/distribution/irradiation/adapter/irradiation/IrradiationResource.java
@@ -12,7 +12,6 @@ import com.arcone.biopro.distribution.irradiation.application.usecase.ValidateUn
 import com.arcone.biopro.distribution.irradiation.domain.repository.ConfigurationService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import lombok.experimental.FieldDefaults;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
