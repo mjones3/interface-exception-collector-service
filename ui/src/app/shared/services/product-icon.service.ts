4e48b0c0195689a1dd6a647307fb0c62af8fcdce
RamBishunkhe <ram.bishunkhe@arc-one.com>
2025-07-22 16:58:06 -0500

    fix(DIS-479):Receive orders and ship washed products

--- ui/src/app/shared/services/product-icon.service.ts
+++ ui/src/app/shared/services/product-icon.service.ts
@@ -79,6 +79,18 @@ export class ProductIconsService {
         {
             productFamily: 'CRYOPRECIPITATE',
             value: 'biopro:cryo',
+        },
+        {
+            productFamily: 'WASHED_APHERESIS_PLATELETS',
+            value: 'biopro:product-platelets',
+        },
+        {
+            productFamily: 'WASHED_PRT_APHERESIS_PLATELETS',
+            value: 'biopro:product-platelets',
+        },
+        {
+            productFamily: 'WASHED_RED_BLOOD_CELLS',
+            value: 'biopro:product-rbc',
         }
     ];
 
