e36ad4937b4c9c7688781416be57769212723b55
Benjamin Pinto <benjamin.pinto@arc-one.com>
2025-07-24 18:16:52 -0400

    feat(DIS-446) - fix mapping product family

--- backend/order/src/test/java/com/arcone/biopro/distribution/order/verification/controllers/OrderTestingController.java
+++ backend/order/src/test/java/com/arcone/biopro/distribution/order/verification/controllers/OrderTestingController.java
@@ -266,8 +266,8 @@ public class OrderTestingController {
             "PRT_APHERESIS_PLATELETS", "PRT Apheresis Platelets",
             "CRYOPRECIPITATE", "Cryoprecipitate",
             "PLASMA_TRANSFUSABLE", "Plasma Transfusable",
-            "PLASMA_MFG_NONINJECTABLE", "Plasma MFG Noninjectable",
-            "PLASMA_MFG_INJECTABLE", "Plasma MFG Injectable"
+            "PLASMA_MFG_NONINJECTABLE", "Plasma Mfg Noninjectable",
+            "PLASMA_MFG_INJECTABLE", "Plasma Mfg Injectable"
         );
         return productFamilyDescription.get(productFamilyKey);
     }
